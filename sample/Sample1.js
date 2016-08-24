34ed351b5d854cd13c04bec97df509bb5342598e
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-08-24 12:24:18 +0300

    sample1 revert

--- sample/Sample1.js
+++ sample/Sample1.js
@@ -3,15 +3,6 @@ require( 'wTools' );
 
 var _ = wTools;
 
-//var arr = _.arrayRange([ 2,10 ]);
+var arr = _.arrayRange([ 2,10 ]);
 
-//console.log( 'arr :',arr );
-
-//returns '', must { routine add }
-console.log(_.toStrMethods((function add(){}),{} ));
-
-//returns { routine add }
-console.log(_.toStrFine((function add(){}),{onlyRoutines:0 } ));
-
-//returns '', must { routine add }
-console.log(_.toStrFine((function add(){}),{onlyRoutines:1 } ));
+console.log( 'arr :',arr );
