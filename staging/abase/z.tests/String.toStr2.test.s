386c10719cc43bfd9d9e5a29bb1a7559aa53ce16
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-08-26 08:44:00 +0300

    toStr test

---
+++ staging/abase/z.tests/String.toStr2.test.s
@@ -0,0 +1,218 @@
+( function _String_toStr2_test_s_( ) {
+
+'use strict';
+
+/*
+
+ to run this test
+ from the project directory run
+
+ npm install
+ node ./staging/abase/z.tests/String.toStr2.test.s
+
+ */
+
+if( typeof module !== 'undefined' )
+{
+
+  require( '../wTools.s' );
+  require( '../component/StringTools.s' );
+
+  if( require( 'fs' ).existsSync( __dirname + '/../object/Testing.debug.s' ) )
+    require( '../object/Testing.debug.s' );
+  else
+    require( 'wTesting' );
+
+}
+
+var _ = wTools;
+var Self = {};
+
+//
+
+var toStr = function( test )
+{
+  var cases =
+  [
+    {
+      desc :  'empty arguments',
+      src : [ {}, '', [] ],
+      options : [ {} ],
+      expected :[ '{}', '""', '[]' ]
+    },
+
+    {
+      desc :  'Symbol test',
+      src :
+      [
+        Symbol(),
+        Symbol('sm'),
+        Symbol('sx'),
+        Symbol('sy')
+
+
+      ],
+      options :
+      [
+        {},
+        {},
+        { levels : 0 },
+        { noAtomic : 1 },
+
+      ],
+      expected :
+      [
+        'Symbol()',
+        'Symbol(sm)',
+        'Symbol(sx)',
+        ''
+      ]
+    },
+
+    {
+      desc :  'Number test',
+      src :
+      [
+        Number(),
+        5,
+        15000,
+        1222.222,
+        1234.4321,
+        15,
+        99,
+        22
+
+
+      ],
+      options :
+      [
+        {},
+        {},
+        { precision : 3 },
+        { fixed : 1 },
+        { noNumber : 1 },
+        { noAtomic : 1 },
+        { levels : 0 },
+        { noRoutine : 1 }
+      ],
+      expected :
+      [
+        '0',
+        '5',
+        '1.50e+4',
+        '1222.2',
+        '',
+        '15',
+        '99',
+        '22'
+
+      ]
+    },
+    {
+      desc : 'Array test',
+      src :
+      [
+        [ 1, 2, 3 ],
+        [ 1, 2, 3 ],
+        [ 1000, 2000, 3000 ],
+        [ 1.1111, 2.2222, 3.3333 ],
+        [ 0, 1, 2 ],
+        [ 'a','b','c', 1, 2, 3 ],
+        [ { a : 1 }, { b : 2 } ],
+        [ { x : 1 }, { y : 2 } ]
+
+
+      ],
+      options :
+      [
+        { },
+        { wrap : 0, comma : '|' },
+        { precision : 2, multiline : 1 },
+        { fixed : 2 },
+        { levels : 0 },
+        { levels : 2, noString : 1 },
+        { },
+        { levels : 2, dtab : '-' }
+
+
+      ],
+      expected :
+      [
+        '[ 1, 2, 3 ]',
+        '  1|2|3',
+        [
+          '[',
+          '  1.0e+3, ',
+          '  2.0e+3, ',
+          '  3.0e+3',
+          ']',
+        ].join( '\n' ),
+        '[ 1.11, 2.22, 3.33 ]',
+        '[ Array with 3 elements ]',
+        '[ 1, 2, 3 ]',
+        [
+          '[',
+          '  [ Object with 1 elements ], ',
+          '  [ Object with 1 elements ]',
+          ']'
+        ].join( '\n' ),
+        [
+          '[',
+          '-{ x : 1 }, ',
+          '-{ y : 2 }',
+          ']'
+        ].join( '\n' )
+      ]
+    },
+
+
+
+
+  ];
+
+  debugger;
+  for( var i = 0; i < cases.length; ++i )
+  {
+    var _case = cases[ i ];
+    var src = _case['src'];
+    var exp = _case['expected'];
+    var o = _case['options'];
+
+    for( var k = 0; k < src.length; ++k  )
+    {
+      test.description = _case.desc;
+      var got = _.toStr( src[ k ], o[ k ] || o[ 0 ] );
+      var expected = exp[ k ];
+      test.identical( got,expected )
+    }
+
+
+  }
+  debugger;
+
+}
+
+// node ./staging/abase/z.tests/String.toStr2.test.s
+
+//
+
+var Proto =
+{
+
+  name : 'toStr',
+
+  tests:
+  {
+      toStr : toStr
+
+
+  }
+
+};
+
+_.mapExtend( Self,Proto );
+
+if( typeof module !== 'undefined' && !module.parent )
+  _.testing.test( Self );
+
+} )( );
