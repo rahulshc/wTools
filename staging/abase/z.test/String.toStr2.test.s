75fccf5ae3c5a6f5ba38fabf3dbf2fcc85a5aadd
wandalen <wandalen.me@gmail.com>
2016-08-31 17:08:52 +0300

    extract char code table from toStr test file

--- staging/abase/z.test/String.toStr2.test.s
+++ staging/abase/z.test/String.toStr2.test.s
@@ -29,30 +29,6 @@ var _ = wTools;
 var Self = {};
 
 //
-/*
-
-\' 	single quote 	byte 0x27 in ASCII encoding
-\" 	double quote 	byte 0x22 in ASCII encoding
-\\ 	backslash 	byte 0x5c in ASCII encoding
-\b 	backspace 	byte 0x08 in ASCII encoding
-\f 	form feed - new page 	byte 0x0c in ASCII encoding
-\n 	line feed - new line 	byte 0x0a in ASCII encoding
-\r 	carriage return 	byte 0x0d in ASCII encoding
-\t 	horizontal tab 	byte 0x09 in ASCII encoding
-\v 	vertical tab 	byte 0x0b in ASCII encoding
-\nnn 	arbitrary octal value 	byte nnn
-\xnn 	arbitrary hexadecimal value 	byte nn
-\unnnn 	universal character name
-(arbitrary Unicode value);
-may result in several characters 	code point U+nnnn
-\Unnnnnnnn 	universal character name
-(arbitrary Unicode value);
-may result in several characters 	code point U+nnnnnnnn
-
-source : http://en.cppreference.com/w/cpp/language/escape
-
-*/
-//
 
 var reportChars = function()
 {
