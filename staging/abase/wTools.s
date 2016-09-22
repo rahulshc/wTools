5a6ae74dd88dd7f212b703c1a763811b2488f9b6
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-09-22 19:52:01 +0300

    Add entityCopyField docs

--- staging/abase/wTools.s
+++ staging/abase/wTools.s
@@ -746,6 +746,25 @@ var entityCopy = function( dst,src,onRecursive )
 
 //
 
+/**
+ * Short-cut for entityCopy function.Copies specified( name ) field from
+ * source container( srcContainer ) into( dstContainer ).
+ *
+ * @param {object} dstContainer - Destination object.
+ * @param {object} srcContainer - Source object.
+ * @param {string} name - Field name.
+ * @param {routine} onRecursive - The callback function to copy each [ key, value ].
+ * @see {@link wTools.mapClone} Check this function for more info about( onRecursive ) callback.
+ * @returns {object} Returns result of entities copy operation.
+ *
+ * @example
+ *
+ *
+ * @method entityCopyField
+ * @memberof wTools
+ *
+ */
+
 var entityCopyField = function( dstContainer,srcContainer,name,onRecursive )
 {
   var result;
