11908b217599388d02654bff6136ed08ad586d5f
volodymyrmigdal <volodymyr.migdal@gmail.com>
2017-07-06 17:18:03 +0300

    add asserts in strRemove* routines

--- staging/abase/akernel/aKernel.s
+++ staging/abase/akernel/aKernel.s
@@ -6971,6 +6971,10 @@ function strEnds( src,end )
 
 function strRemoveBegin( src,begin )
 {
+  _.assert( arguments.length === 2 );
+  _.assert( _.arrayLike( src ) || _.strIs( src ) );
+  _.assert( _.arrayLike( begin ) || _.strIs( begin ) );
+
   begin = _.arrayAs( begin );
 
   var result = _.arrayAs( src ).slice();
@@ -7016,6 +7020,10 @@ function strRemoveBegin( src,begin )
 
 function strRemoveEnd( src,end )
 {
+  _.assert( arguments.length === 2 );
+  _.assert( _.arrayLike( src ) || _.strIs( src ) );
+  _.assert( _.arrayLike( end ) || _.strIs( end ) );
+
   end = _.arrayAs( end );
 
   var result = _.arrayAs( src ).slice();
