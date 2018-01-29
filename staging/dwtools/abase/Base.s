1ecf862519d8d7162aec375cb09d86bdd357f89b
wandalen <wandalen.me@gmail.com>
2018-01-29 10:56:15 +0400

    publish

---
+++ staging/dwtools/abase/Base.s
@@ -0,0 +1,13 @@
+(function _Base_s_() {
+
+'use strict';
+
+if( typeof module !== 'undefined' && module !== null )
+{
+
+  require( './Layer2.s' );
+  module[ 'exports' ] = _global_.wTools;
+
+}
+
+})();
