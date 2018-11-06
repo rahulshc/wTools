41f58caeae53ed3b21a8c128a67ddeefa9071345
Wandalen <wandalen.me@gmail.com>
2018-11-06 16:56:49 +0200

    auto

--- out.debug/dwtools/abase/l1.test/Routine.test.s
+++ out.debug/dwtools/abase/l1.test/Routine.test.s
@@ -1659,6 +1659,6 @@ var Self =
 
 Self = wTestSuite( Self );
 if( typeof module !== 'undefined' && !module.parent )
-/*_.*/wTester.test( Self.name );
+wTester.test( Self.name );
 
 } )( );
--- proto/dwtools/abase/l1.test/Array.test.s
+++ proto/dwtools/abase/l1.test/Array.test.s
@@ -12613,6 +12613,6 @@ var Self =
 
 Self = wTestSuite( Self );
 if( typeof module !== 'undefined' && !module.parent )
-/*_.*/wTester.test( Self.name );
+wTester.test( Self.name );
 
 })();
