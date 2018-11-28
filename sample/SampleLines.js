48cdd9543a70af693d052efc3fde46ec3d83eac5
Pablo Garcimartin <pablogarcimartinruiz@gmail.com>
2018-11-28 14:19:00 +0100

    Sample strLinesStrip

--- sample/SampleLines.js
+++ sample/SampleLines.js
@@ -4,27 +4,10 @@ require( 'wTools' );
 
 var _ = wTools;
 
-var srcStr =
-`\na\nbc\ndef\nghij\n\n`
-;
+var srcStr = `\na\nbc\ndef\nghij\n\n`;
 
-var crange = [ 3, 5];
 
-var expected =
-[
-  '',
-  'def',
-  '',
-]
-
-var got = _.strLinesNearest
-({
-  src : srcStr,
-  charsRange : 3,
-  numberOfLines : 2,
-});
-
-console.log( 'String' )
-console.log( 'GOT',  [ _.strLinesStrip( '  Hello \r\n\t', ' World \n\n  ! \n\n', '\n\n' ) ]  , 'end'  );
-console.log( 'Array' )
-console.log( 'GOT', _.strLinesStrip( [ '  Hello \r\n\t world \n\n ', '\n! \n' ], [ '\n\nHow\n\nAre  ', '  \r\nyou \n? \n'] ), 'end'  );
+console.log( 'Input is a string' )
+console.log( 'GOT',  [ _.strLinesStrip( '  Hello \r\n\t World \n\n  ! \n\n' ) ] );
+console.log( 'Input is an Array' )
+console.log( 'GOT',    _.strLinesStrip( [ '  Hello \r\n\t World \n\n  ! \n\n' ] ) );
