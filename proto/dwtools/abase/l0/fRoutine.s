e68f14fa4385f0b5b4471fada7c769cd558a595d
Wandalen <wandalen.me@gmail.com>
2019-06-28 10:33:05 +0300

    auto commit

--- proto/dwtools/abase/l0/fRoutine.s
+++ proto/dwtools/abase/l0/fRoutine.s
@@ -651,18 +651,17 @@ Is it bug or normal work?
 
 /**
  * The routineExtend() is used to copy the values of all properties
- * from source routine (source map options) to a target routine.
+ * from source routine to a target routine.
  *
- * It takes first routine (dst), and check every property of it.
- * Then it checks properties of source routine and extends dst by its.
+ * It takes first routine (dst), and shallow clone each destination property of type map.
+ * Then it checks properties of source routine (src) and extends dst by source properties.
  * The dst properties can be owerwriten by values of source routine
  * if descriptor (writable) of dst property is set.
  *
- * If the first routine (dst) is null,
- * the routineExtend() can make a routine from source routine (source map options),
- * which have pre and body routines in properties
+ * If the first routine (dst) is null then
+ * routine routineExtend() makes a routine from routines pre and body
  * @see {@link wTools.routineFromPreAndBody} - Automatic routine generating
- * from preparation vroutine and main routine (body).
+ * from preparation routine and main routine (body).
  *
  * @param{ routine } dst - The target routine or null.
  * @param{ * } src - The source routine or object to copy.
@@ -673,7 +672,7 @@ Is it bug or normal work?
  * {
  *   pre : _.routinesCompose.pre,
  *   body : _.routinesCompose.body,
- *   option : 1,
+ *   someOption : 1,
  * }
  * var got = _.routineExtend( null, src );
  *
@@ -682,13 +681,13 @@ Is it bug or normal work?
  * _.routineExtend( null, _.routinesCompose );
  *
  * @example
- * // returns [ routine routinesCompose ], routinesCompose.option === 1
- * _.routineExtend( _.routinesCompose, { option : 1 } );
+ * // returns [ routine routinesCompose ], routinesCompose.someOption === 1
+ * _.routineExtend( _.routinesCompose, { someOption : 1 } );
  *
  * @example
- * // returns [ routine routinesCompose ], routinesCompose.option === 1
- * _.routinesComposes.option = 22;
- * _.routineExtend( _.routinesCompose, { option : 1 } );
+ * // returns [ routine routinesCompose ], routinesCompose.someOption === 1
+ * _.routinesComposes.someOption = 22;
+ * _.routineExtend( _.routinesCompose, { someOption : 1 } );
  *
  * @returns { routine } It will return the target routine with extended properties.
  * @function routineExtend
