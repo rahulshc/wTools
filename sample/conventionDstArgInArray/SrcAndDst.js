b23dbfb31abd61f6ea11d1e22981e7a9e2b53aad
Dmytro <dm.vict.kr@gmail.com>
2019-07-17 16:31:34 +0300

    Add samples

---
+++ sample/conventionDstArgInArray/SrcAndDst.js
@@ -0,0 +1,35 @@
+require( 'wTools' );
+
+// original array
+
+let origin = [ 1, 2, 'a', 'b', true, 1,  [ 1 ] ];
+
+// second argument in the routines
+
+let elem = 1;
+
+// routine uses argument srcArray
+
+let srcArray = wTools.longSlice( origin );
+
+let result = wTools.arrayCountElement( srcArray, elem );
+
+console.log( 'The result is:' );
+console.log( result );
+console.log( 'The original array is:');
+console.log( origin );
+console.log( 'srcArray changed to:' );
+console.log( srcArray );
+
+// routine uses argument dstArray
+
+let dstArray = wTools.longSlice( origin );
+
+let result1 = wTools.arrayAppend( dstArray, elem );
+
+console.log( 'The result is:' );
+console.log( result1 );
+console.log( 'The original array is:');
+console.log( origin );
+console.log( 'dstArray changed to:' );
+console.log( dstArray );
---
+++ sample/routineFromPreAndBody/Name.js
@@ -0,0 +1,46 @@
+let _ = require( 'wTools' );
+
+//
+
+function name_pre( routine, args )
+{
+  let o = args[ 0 ];
+  if( _.strIs( o ) )
+  o = { path : o };
+
+  _.routineOptions( routine, o );
+  _.assert( o && _.strIs( o.path ), 'Expects strings {-o.path-}' );
+  _.assert( args.length === 1 );
+  _.assert( arguments.length === 2 );
+
+  return o;
+}
+
+function name_body( o )
+{
+  let i = o.path.lastIndexOf( '/' );
+  if( i !== -1 )
+  o.path = o.path.substr( i+1 );
+
+  if( !o.full )
+  {
+    let i = o.path.lastIndexOf( '.' );
+    if( i !== -1 ) o.path = o.path.substr( 0, i );
+  }
+
+  return o.path;
+}
+
+name_body.defaults =
+{
+  path : null,
+  full : 0,
+}
+
+let name = _.routineFromPreAndBody( name_pre, name_body );
+
+let nameFull = _.routineFromPreAndBody( name_pre, name_body );
+nameFull.defaults.full = 1;
+
+console.log( name( './a/b/c/File.js' ) );
+console.log( nameFull( './a/b/c/File.js' ) );
