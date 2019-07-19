fc06ec71c4c6dac69517557acbee1fc0cc4ab5af
Dmytro <dm.vict.kr@gmail.com>
2019-07-19 08:18:39 +0300

    Add JSdoc to routine eachSample()

--- proto/dwtools/abase/l0/fEntity.s
+++ proto/dwtools/abase/l0/fEntity.s
@@ -83,25 +83,42 @@ function multipleAll( dsts )
 // --
 
 /**
- * Routine eachSample() accepts the option {-o.sets-} which contains scalar or vector
- * elements. Routine returns an array of vectors each of which is unique combination
- * of vector elements passed in option {-o.sets-}.
+ * Routine eachSample() accepts the container {-o.sets-} with scalar or vector elements.
+ * Routine returns an array of vectors. Each vector is a unique combination of elements of vectors
+ * that is passed in option {-o.sets-}.
  *
- * Routine eachSample() accepts the map option {-o-} or two arguments. If map options
+ * Routine eachSample() accepts the options map {-o-} or two arguments. If options map
  * is used, all parameters can be set. If passed two arguments, first of them is ( sets )
- * and second is onEach.
+ * and second is ( onEach ).
  *
  * @param {Array|Map} sets - Container with vector and scalar elements to combine new vectors.
- * @param {Routine|Null} onEach - Callback. Should not change element of {-sets-}.
+ * @param {Routine|Null} onEach - Callback. Should not change elements of {-sample-}.
  * @param {Array|Map} sample - Container for new vectors. If not passed, routine create empty container.
  * @param {boolean} leftToRight - Sets the direction of combining. 1 - left to rigth, 0 - rigth to left. By default is 1.
- * @param {boolean} result - Set retuned value. 1 - array with verctors, 0 - index of last element. By default is 1.
+ * @param {boolean} result - Sets retuned value. 1 - array with verctors, 0 - index of last element. By default is 1.
+ *
+ * @example
+ * //returns  [ [ 0, 2 ], [ 1, 2 ] ]
+ * var got = _.eachSample( { sets : [ [ 0, 1 ], 2 ] });
  *
  * @example
  * //returns  [ [ 0, 2 ], [ 1, 2 ],
- *              [ 0, 3 ], [ 1, 3 ] ];
+ *              [ 0, 3 ], [ 1, 3 ] ]
  * var got = _.eachSample( { sets : [ [ 0, 1 ], [ 2, 3 ] ] });
  *
+ * @example
+ * //returns  [ { a : 0, b : 2}, { a : 1, b : 2},
+ *              { a : 0, b : 3}, { a : 1, b : 3} ]
+ * var got = _.eachSample( { sets : { a : [ 0, 1 ], b : [ 2, 3 ] } });
+ *
+ * @example
+ * //returns  [ [ 3, 0 ], [ 2, 0 ],
+ *              [ 3, 1 ], [ 2, 1 ] ]
+ * var got = _.eachSample( { sets : [ [ 0, 1 ], [ 2, 3 ] ], leftToRight : 0 });
+ *
+ * @example
+ * //returns  3
+ * var got = _.eachSample( { sets : [ [ 0, 1 ], [ 2, 3 ] ], result : 0 });
  *
  * @function eachSample
  * @returns {Array} Returns array contained  check function.
@@ -109,7 +126,7 @@ function multipleAll( dsts )
  * @throws {exception} If( onEach ) is not a Routine or null.
  * @throws {exception} If( o.sets ) is not array or objectLike.
  * @throws {exception} If ( sets ) is mapLike and ( onEach ) not passed.
- * @throws {exception} If( o.base ) and ( o.add) is undefined.
+ * @throws {exception} If( o.base ) or ( o.add) is undefined.
  * @memberof wTools
  */
 
