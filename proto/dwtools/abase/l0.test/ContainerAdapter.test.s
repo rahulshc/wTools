17f55467c5328630c3a29accffea0ae14108e4d8
Dmytro <dm.vict.kr@gmail.com>
2019-10-11 15:08:46 +0300

    Implemented test routine arrayAdapterRemovedOnce

--- proto/dwtools/abase/l0.test/ContainerAdapter.test.s
+++ proto/dwtools/abase/l0.test/ContainerAdapter.test.s
@@ -10068,6 +10068,108 @@ function arrayAdapterRemoved( test )
 
 //
 
+function arrayAdapterRemovedOnce( test )
+{
+  test.case = 'empty container, remove primitive';
+  var dst = _.containerAdapter.make( [] );
+  var got = dst.removedOnce( 1 );
+  var exp = [];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+
+  test.case = 'empty container, remove Long';
+  var dst = _.containerAdapter.make( [] );
+  var got = dst.removedOnce( _.unrollMake( [ 1, 2 ] ) );
+  var exp = [];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+
+  test.case = 'empty container, remove map';
+  var dst = _.containerAdapter.make( [] );
+  var got = dst.removedOnce( { a : 0 } );
+  var exp = [];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+
+  /* */
+
+  test.case = 'container, remove primitive';
+  var dst = _.containerAdapter.make( [ 1, 1, 2, 2, '1' ] );
+  var got = dst.removedOnce( 1 );
+  var exp = [ 1, 2, 2, '1' ];
+  test.identical( got, 0 );
+  test.identical( dst.original, exp );
+
+  test.case = 'container, remove Long';
+  var dst = _.containerAdapter.make( [ 1, 1, 2, 2, '1' ] );
+  var got = dst.removedOnce( _.unrollMake( [ 1, 2 ] ) );
+  var exp = [ 1, 1, 2, 2, '1' ];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+
+  test.case = 'container, remove map';
+  var dst = _.containerAdapter.make( [ 1, 1, 2, 2, '1' ] );
+  var got = dst.removedOnce( { a : 1 } );
+  var exp = [ 1, 1, 2, 2, '1' ];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+
+  /* */
+
+  test.case = 'container, remove primitive, duplicates';
+  var dst = _.containerAdapter.make( [ 1, 1, 2, 2, 3, 3 ] );
+  var got = dst.removedOnce( 1 );
+  var exp = [ 1, 2, 2, 3, 3 ];
+  test.identical( got, 0 );
+  test.identical( dst.original, exp );
+
+  test.case = 'container, remove Long, duplicates';
+  var dst = _.containerAdapter.make( [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ] );
+  var got = dst.removedOnce( [ 1, 2 ] );
+  var exp = [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+
+  test.case = 'container, remove map, duplicates';
+  var dst = _.containerAdapter.make( [ { a : 0 }, { a : 0 }, { a : 0 }, { a : 1 } ] );
+  var got = dst.removedOnce( { a : 0 } );
+  var exp = [ { a : 0 }, { a : 0 }, { a : 0 }, { a : 1 } ];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+
+  /* */
+
+  test.case = 'container, remove Long, one evaluator';
+  var dst = _.containerAdapter.make( [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ] );
+  var got = dst.removedOnce( [ 1, 2 ], ( e ) => e[ 0 ] );
+  var exp = [ [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ];
+  test.identical( got, 0 );
+  test.identical( dst.original, exp );
+
+  test.case = 'container, remove Long, two evaluators';
+  var dst = _.containerAdapter.make( [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ] );
+  var got = dst.removedOnce( [ 1, 2 ], ( e ) => e[ 0 ], ( ins ) => ins[ 0 ] );
+  var exp = [ [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ];
+  test.identical( got, 0 );
+  test.identical( dst.original, exp );
+
+  test.case = 'container, remove Long, fromIndex and evaluator2';
+  var dst = _.containerAdapter.make( [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ] );
+  var got = dst.removedOnce( [ 1, 2 ], 2, ( e ) => e[ 0 ] );
+  var exp = [ [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ];
+  test.identical( got, 2 );
+  test.identical( dst.original, exp );
+
+  test.case = 'container, remove Long, equalizer';
+  var dst = _.containerAdapter.make( [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ] );
+  var got = dst.removedOnce( [ 1, 2 ], ( e, ins ) => e === ins[ 0 ] );
+  var exp = [ [ 1, 2 ], [ 1, 2 ], [ 1, 2 ], [ 2, 2 ] ];
+  test.identical( got, -1 );
+  test.identical( dst.original, exp );
+}
+
+//
+
 function arrayAdapterRemove( test )
 {
   test.case = 'empty container, remove primitive';
@@ -11425,6 +11527,7 @@ var Self =
     arrayAdapterAppendContainer,
     arrayAdapterAppendContainerOnce,
     arrayAdapterRemoved,
+    arrayAdapterRemovedOnce,
     arrayAdapterRemove,
     arrayAdapterRemoveOnce,
     arrayAdapterRemoveOnceStrictly,
