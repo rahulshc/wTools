41da46d7c5528be67abcf0321040cec305ea228a
Dmytro <dm.vict.kr@gmail.com>
2019-10-16 18:05:04 +0300

    Implement routine scalarPrependOnce

--- proto/dwtools/abase/l0/l6/fFunctional.s
+++ proto/dwtools/abase/l0/l6/fFunctional.s
@@ -123,6 +123,55 @@ function scalarAppendOnce( dst, src )
 
 //
 
+function scalarPrependOnce( dst, src )
+{
+
+  _.assert( arguments.length === 2 );
+
+  if( dst === undefined )
+  {
+    if( _.longIs( src ) )
+    {
+      dst = [];
+    }
+    else
+    {
+      if( src === undefined )
+      return [];
+      else
+      return src;
+    }
+  }
+
+  if( _.longIs( dst ) )
+  {
+
+    if( !_.arrayIs( dst ) )
+    dst = _.arrayFrom( dst );
+
+    if( src === undefined )
+    {}
+    else if( _.longIs( src ) )
+    _.arrayPrependArrayOnce( dst, src );
+    else
+    _.arrayPrependElementOnce( dst, src );
+
+  }
+  else
+  {
+
+    if( src === undefined )
+    {}
+    else if( _.longIs( src ) )
+    dst = _.arrayPrependArrayOnce( [ dst ], src );
+    else
+    dst = _.arrayPrependElementOnce( [ dst ], src );
+
+  }
+
+  return dst;
+}
+
 /**
  * The scalarToVector() routine returns a new array
  * which containing the static elements only type of Number.
