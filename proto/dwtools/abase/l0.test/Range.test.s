81220982b89b79abedc9689d857d40bb098a70eb
Dmytro <dm.vict.kr@gmail.com>
2019-10-26 09:25:08 +0300

    Implement test routine rangeInExclusive

--- proto/dwtools/abase/l0.test/Range.test.s
+++ proto/dwtools/abase/l0.test/Range.test.s
@@ -86,7 +86,7 @@ function rangeIs( test )
   var expected = false;
   test.identical( got, expected );
 
-  if( Config.interpreter === 'njs' )
+  if( Config.interpreter = 'njs' )
   {
     test.case = 'BufferNode';
     var got = _.rangeIs( BufferNode.alloc( 5 ) );
@@ -315,7 +315,7 @@ function rangeIsEmpty( test )
   var expected = false;
   test.identical( got, expected );
 
-  if( Config.interpreter === 'njs' )
+  if( Config.interpreter = 'njs' )
   {
     test.case = 'BufferNode';
     var got = _.rangeIsEmpty( BufferNode.alloc( 5 ) );
@@ -564,7 +564,7 @@ function rangeIsPopulated( test )
   var expected = false;
   test.identical( got, expected );
 
-  if( Config.interpreter === 'njs' )
+  if( Config.interpreter = 'njs' )
   {
     test.case = 'BufferNode';
     var got = _.rangeIsPopulated( BufferNode.alloc( 5 ) );
@@ -748,9 +748,19 @@ function rangeInInclusive( test )
   var expected = false;
   test.identical( got, expected );
 
+  test.case = 'srcNumber - number, srcNumber = range[ 0 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], 2 );
+  var expected = true;
+  test.identical( got, expected );
+
   test.case = 'srcNumber - number, srcNumber > range[ 1 ]';
   var got = _.rangeInInclusive( [ 2, 5 ], 7 );
   var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - number, srcNumber = range[ 1 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], 5 );
+  var expected = true;
   test.identical( got, expected );
 
   test.case = 'srcNumber - number, range[ 0 ] < srcNumber < range[ 1 ]';
@@ -763,9 +773,19 @@ function rangeInInclusive( test )
   var expected = false;
   test.identical( got, expected );
 
+  test.case = 'srcNumber - array, srcNumber = range[ 0 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], [ 0, 0 ] );
+  var expected = true;
+  test.identical( got, expected );
+
   test.case = 'srcNumber - array, srcNumber > range[ 1 ]';
   var got = _.rangeInInclusive( [ 2, 5 ], [ 0, 0, 0, 0, 0, 0, 0 ] );
   var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - array, srcNumber = range[ 1 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], [ 0, 0, 0, 0, 0 ] );
+  var expected = true;
   test.identical( got, expected );
 
   test.case = 'srcNumber - array, range[ 0 ] < srcNumber < range[ 1 ]';
@@ -778,9 +798,19 @@ function rangeInInclusive( test )
   var expected = false;
   test.identical( got, expected );
 
+  test.case = 'srcNumber - unroll, srcNumber = range[ 0 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], _.unrollMake( [ 0, 0 ] ) );
+  var expected = true;
+  test.identical( got, expected );
+
   test.case = 'srcNumber - unroll, srcNumber > range[ 1 ]';
   var got = _.rangeInInclusive( [ 2, 5 ], _.unrollMake( [ 0, 0, 0, 0, 0, 0, 0 ] ) );
   var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - unroll, srcNumber = range[ 1 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], _.unrollMake( [ 0, 0, 0, 0, 0 ] ) );
+  var expected = true;
   test.identical( got, expected );
 
   test.case = 'srcNumber - unroll, range[ 0 ] < srcNumber < range[ 1 ]';
@@ -793,9 +823,19 @@ function rangeInInclusive( test )
   var expected = false;
   test.identical( got, expected );
 
+  test.case = 'srcNumber - argumentsArray, srcNumber = range[ 0 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0, 0 ] ) );
+  var expected = true;
+  test.identical( got, expected );
+
   test.case = 'srcNumber - argumentsArray, srcNumber > range[ 1 ]';
   var got = _.rangeInInclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0, 0, 0, 0, 0, 0, 0 ] ) );
   var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - argumentsArray, srcNumber = range[ 1 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0, 0, 0, 0, 0 ] ) );
+  var expected = true;
   test.identical( got, expected );
 
   test.case = 'srcNumber - argumentsArray, range[ 0 ] < srcNumber < range[ 1 ]';
@@ -808,9 +848,19 @@ function rangeInInclusive( test )
   var expected = false;
   test.identical( got, expected );
 
+  test.case = 'srcNumber - BufferTyped, srcNumber = range[ 0 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], new I8x( [ 0, 0 ] ) );
+  var expected = true;
+  test.identical( got, expected );
+
   test.case = 'srcNumber - BufferTyped, srcNumber > range[ 1 ]';
   var got = _.rangeInInclusive( [ 2, 5 ], new U16x( [ 0, 0, 0, 0, 0, 0, 0 ] ) );
   var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - BufferTyped, srcNumber = range[ 1 ]';
+  var got = _.rangeInInclusive( [ 2, 5 ], new U16x( [ 0, 0, 0, 0, 0 ] ) );
+  var expected = true;
   test.identical( got, expected );
 
   test.case = 'srcNumber - BufferTyped, range[ 0 ] < srcNumber < range[ 1 ]';
@@ -839,6 +889,156 @@ function rangeInInclusive( test )
   test.shouldThrowErrorSync( () => _.rangeInInclusive( [ 1, 2 ], 'wrong' ) );
 }
 
+//
+
+function rangeInExclusive( test )
+{
+  test.case = 'srcNumber - number, srcNumber < range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], 1 );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - number, srcNumber = range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], 2 );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - number, srcNumber > range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], 7 );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - number, srcNumber = range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], 5 );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - number, range[ 0 ] < srcNumber < range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], 4 );
+  var expected = true;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - array, srcNumber < range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], [ 0 ] );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - array, srcNumber = range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], [ 0, 0 ] );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - array, srcNumber > range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], [ 0, 0, 0, 0, 0, 0, 0 ] );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - array, srcNumber = range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], [ 0, 0, 0, 0, 0 ] );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - array, range[ 0 ] < srcNumber < range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], [ 0, 0, 0, 0 ] );
+  var expected = true;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - unroll, srcNumber < range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.unrollMake( [ 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - unroll, srcNumber = range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.unrollMake( [ 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - unroll, srcNumber > range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.unrollMake( [ 0, 0, 0, 0, 0, 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - unroll, srcNumber = range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.unrollMake( [ 0, 0, 0, 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - unroll, range[ 0 ] < srcNumber < range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.unrollMake( [ 0, 0, 0, 0 ] ) );
+  var expected = true;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - argumentsArray, srcNumber < range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - argumentsArray, srcNumber = range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - argumentsArray, srcNumber > range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0, 0, 0, 0, 0, 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - argumentsArray, srcNumber = range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0, 0, 0, 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - argumentsArray, range[ 0 ] < srcNumber < range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], _.argumentsArrayMake( [ 0, 0, 0, 0 ] ) );
+  var expected = true;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - BufferTyped, srcNumber < range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], new I8x( [ 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - BufferTyped, srcNumber = range[ 0 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], new I8x( [ 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - BufferTyped, srcNumber > range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], new U16x( [ 0, 0, 0, 0, 0, 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - BufferTyped, srcNumber = range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], new U16x( [ 0, 0, 0, 0, 0 ] ) );
+  var expected = false;
+  test.identical( got, expected );
+
+  test.case = 'srcNumber - BufferTyped, range[ 0 ] < srcNumber < range[ 1 ]';
+  var got = _.rangeInExclusive( [ 2, 5 ], new F32x( [ 0, 0, 0, 0 ] ) );
+  var expected = true;
+  test.identical( got, expected );
+
+  /* - */
+
+  if( !Config.debug )
+  return;
+
+  test.case = 'without arguments';
+  test.shouldThrowErrorSync( () => _.rangeInExclusive() );
+
+  test.case = 'not enough arguments';
+  test.shouldThrowErrorSync( () => _.rangeInExclusive( [ 1, 2 ] ) );
+
+  test.case = 'extra arguments';
+  test.shouldThrowErrorSync( () => _.rangeInExclusive( [ 1, 2 ], 3, 'extra' ) );
+
+  test.case = 'range is not Range';
+  test.shouldThrowErrorSync( () => _.rangeInExclusive( 'wrong', 3 ) );
+
+  test.case = 'srcNumber is not Long, not Number';
+  test.shouldThrowErrorSync( () => _.rangeInExclusive( [ 1, 2 ], 'wrong' ) );
+}
+
 // --
 // declaration
 // --
@@ -860,6 +1060,7 @@ var Self =
     rangeIsPopulated,
 
     rangeInInclusive,
+    rangeInExclusive,
 
   }
 
