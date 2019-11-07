195dabcccda0a8f0aeb1f151be2787c57fb2f56a
Dmytro <dm.vict.kr@gmail.com>
2019-11-07 12:18:44 +0200

    Implement test routine nothingIs

--- proto/dwtools/abase/l0.test/Typing.test.s
+++ proto/dwtools/abase/l0.test/Typing.test.s
@@ -15,6 +15,95 @@ var _ = _global_.wTools;
 // tests
 // --
 
+function nothingIs( test )
+{
+  test.case = 'without argument';
+  var got = _.nothingIs();
+  test.identical( got, true );
+
+  test.case = 'check null';
+  var got = _.nothingIs( null );
+  test.identical( got, true );
+
+  test.case = 'check undefined';
+  var got = _.nothingIs( undefined );
+  test.identical( got, true );
+
+  test.case = 'check _.nothing';
+  var got = _.nothingIs( _.nothing );
+  test.identical( got, true );
+
+  /* */
+
+  test.case = 'check zero';
+  var got = _.nothingIs( 0 );
+  test.identical( got, false );
+
+  test.case = 'check empty string';
+  var got = _.nothingIs( '' );
+  test.identical( got, false );
+
+  test.case = 'check false';
+  var got = _.nothingIs( false );
+  test.identical( got, false );
+
+  test.case = 'check NaN';
+  var got = _.nothingIs( NaN );
+  test.identical( got, false );
+
+  test.case = 'check empty array';
+  var got = _.nothingIs( [] );
+  test.identical( got, false );
+
+  test.case = 'check empty arguments array';
+  var got = _.nothingIs( _.argumentsArrayMake( [] ) );
+  test.identical( got, false );
+
+  test.case = 'check empty unroll';
+  var got = _.nothingIs( _.unrollMake( [] ) );
+  test.identical( got, false );
+
+  test.case = 'check empty map';
+  var got = _.nothingIs( {} );
+  test.identical( got, false );
+
+  test.case = 'check empty pure map';
+  var got = _.nothingIs( Object.create( null ) );
+  test.identical( got, false );
+
+  test.case = 'check empty Set';
+  var got = _.nothingIs( new Set( [] ) );
+  test.identical( got, false );
+
+  test.case = 'check empty Map';
+  var got = _.nothingIs( new Map( [] ) );
+  test.identical( got, false );
+
+  test.case = 'check empty BufferRaw';
+  var got = _.nothingIs( new BufferRaw() );
+  test.identical( got, false );
+
+  test.case = 'check empty BufferTyped';
+  var got = _.nothingIs( new U8x() );
+  test.identical( got, false );
+
+  test.case = 'check number';
+  var got = _.nothingIs( 3 );
+  test.identical( got, false );
+
+  test.case = 'check string';
+  var got = _.nothingIs( 'str' );
+  test.identical( got, false );
+
+  test.case = 'check not empty array';
+  var got = _.nothingIs( [ null ] );
+  test.identical( got, false );
+
+  test.case = 'check not empty map';
+  var got = _.nothingIs( { '' : null } );
+  test.identical( got, false );
+}
+
 function objectLike( t )
 {
 
@@ -39,24 +128,6 @@ function objectLike( t )
 
 //
 
-function promiseIs( test )
-{
-  test.case = 'check if entity is a Promise';
-
-  test.is( !_.promiseIs() );
-  test.is( !_.promiseIs( {} ) );
-
-  var _Promise = function Promise(){};
-  test.is( !_.promiseIs( new _Promise() ) );
-
-  test.is( _.promiseIs( Promise.resolve( 0 ) ) );
-
-  var promise = new Promise( ( resolve, reject ) => { resolve( 0 ) } )
-  test.is( _.promiseIs( promise ) );
-}
-
-//
-
 function consequenceLike( test )
 {
   test.case = 'check if entity is a consequenceLike';
@@ -71,6 +142,24 @@ function consequenceLike( test )
   var promise = new Promise( ( resolve, reject ) => { resolve( 0 ) } )
   test.is( _.consequenceLike( promise ) );
 
+}
+
+//
+
+function promiseIs( test )
+{
+  test.case = 'check if entity is a Promise';
+
+  test.is( !_.promiseIs() );
+  test.is( !_.promiseIs( {} ) );
+
+  var _Promise = function Promise(){};
+  test.is( !_.promiseIs( new _Promise() ) );
+
+  test.is( _.promiseIs( Promise.resolve( 0 ) ) );
+
+  var promise = new Promise( ( resolve, reject ) => { resolve( 0 ) } )
+  test.is( _.promiseIs( promise ) );
 }
 
 //
@@ -161,9 +250,12 @@ var Self =
   tests :
   {
 
+    nothingIs,
+
     objectLike,
-    promiseIs,
     consequenceLike,
+    promiseIs,
+
     isPrototypeOf,
 
   }
