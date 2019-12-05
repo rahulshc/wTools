db5cc60eb7db9dd0b34ff4342b3321f5d4b2b1cd
Wandalen <wandalen.me@gmail.com>
2019-12-05 21:49:09 +0200

    version 0.8.523

---
+++ proto/dwtools/abase/l0/l8/gSet.s
@@ -0,0 +1,33 @@
+( function _gSet_s_() {
+
+'use strict';
+
+let _global = _global_;
+let _ = _global_.wTools;
+let Self = _;
+// let Self = _.set = _.set || Object.create( null );
+
+// --
+// array set
+// --
+
+// --
+// routines
+// --
+
+let NamespaceExtension =
+{
+}
+
+//
+
+Object.assign( Self, NamespaceExtension );
+
+// --
+// export
+// --
+
+if( typeof module !== 'undefined' && module !== null )
+module[ 'exports' ] = Self;
+
+})();
