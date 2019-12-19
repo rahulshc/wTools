f6d3aa07ca0e95cbd324ef205bbb0f28498b3ee8
Dmytro <dm.vict.kr@gmail.com>
2019-12-19 12:28:14 +0200

    Implement trivial test routine `_finally`, time namespace

--- proto/dwtools/abase/l0.test/Time.test.s
+++ proto/dwtools/abase/l0.test/Time.test.s
@@ -28,7 +28,7 @@ function _begin( test )
   test.identical( got.result, undefined );
 
   test.case = 'delay - undefined, onTime';
-  var onTime = () => 0;  
+  var onTime = () => 0; 
   var got = _.time._begin( undefined, onTime );
   got.time();
   test.identical( got.onTime, onTime );
@@ -199,6 +199,95 @@ function _begin( test )
 
 //
 
+function _finally( test ) 
+{
+  test.open( 'delay - undefined, execute callbacks' );
+
+  test.case = 'delay - undefined';
+  var got = _.time._finally( undefined, undefined );
+  got.time();
+  test.identical( got.onTime, undefined );
+  test.identical( got.onCancel, undefined );
+  test.identical( got.state, 2 );
+  test.identical( got.result, undefined );
+
+  test.case = 'delay - undefined, onTime';
+  var onTime = () => 0; 
+  var got = _.time._finally( undefined, onTime );
+  got.time();
+  test.identical( got.onTime, onTime );
+  test.identical( got.onCancel, onTime );
+  test.identical( got.state, 2 );
+  test.identical( got.result, 0 );
+
+  test.case = 'delay - undefined, onTime';
+  var onTime = () => 0; 
+  var got = _.time._finally( undefined, onTime );
+  got.cancel();
+  test.identical( got.onTime, onTime );
+  test.identical( got.onCancel, onTime );
+  test.identical( got.state, -2 );
+  test.identical( got.result, 0 );
+
+  test.close( 'delay - undefined, execute callbacks' );
+
+  /* - */
+
+  test.open( 'delay - 0, execute callbacks' );
+
+  test.case = 'delay - 0';
+  var got = _.time._finally( 0, undefined );
+  got.time();
+  test.identical( got.onTime, undefined );
+  test.identical( got.onCancel, undefined );
+  test.identical( got.state, 2 );
+  test.identical( got.result, undefined );
+
+  test.case = 'delay - 0, onTime';
+  var onTime = () => 0;  
+  var got = _.time._finally( 0, onTime );
+  got.time();
+  test.identical( got.onTime, onTime );
+  test.identical( got.onCancel, onTime );
+  test.identical( got.state, 2 );
+  test.identical( got.result, 0 );
+
+  test.case = 'delay - 0, onTime';
+  var onTime = () => 0;  
+  var got = _.time._finally( 0, onTime );
+  got.cancel();
+  test.identical( got.onTime, onTime );
+  test.identical( got.onCancel, onTime );
+  test.identical( got.state, -2 );
+  test.identical( got.result, 0 );
+
+  test.close( 'delay - 0, execute callbacks' );
+
+  /* - */
+  
+  test.open( 'delay - 1, not execute callbacks' );
+
+  test.case = 'delay - 1';
+  var got = _.time._finally( 1, undefined );
+  got.original;
+  test.identical( got.onTime, undefined );
+  test.identical( got.onCancel, undefined );
+  test.identical( got.state, 0 );
+  test.identical( got.result, undefined );
+
+  test.case = 'delay - 1, onTime';
+  var onTime = () => 0;  
+  var got = _.time._finally( 0, onTime );
+  test.identical( got.onTime, onTime );
+  test.identical( got.onCancel, onTime );
+  test.identical( got.state, 0 );
+  test.identical( got.result, undefined );
+
+  test.close( 'delay - 1, not execute callbacks' );
+}
+
+//
+
 function timeOutCancelInsideOfCallback( test )
 {
   let context = this;
@@ -306,6 +395,7 @@ var Self =
   tests :
   {
     _begin,
+    _finally,
 
     timeOutCancelInsideOfCallback,
     timeOutCancelOutsideOfCallback,
