941c8e008a60f152477ffad2a10caa5973ef2ba2
Dmytro <dm.vict.kr@gmail.com>
2020-03-16 16:48:03 +0200

    Reimplement routine `strReplace`, use iterative method

--- proto/dwtools/abase/l0/l5/fStr.s
+++ proto/dwtools/abase/l0/l5/fStr.s
@@ -990,6 +990,54 @@ function strReplaceEnd( src, end, ins )
 // 
 //   return result;
 // }
+// 
+// function strReplace( src, ins, sub ) /* qqq2 : ask */
+// {
+//   _.assert( arguments.length === 3, 'Expects exactly three arguments' );
+//   _.assert( _.strsAreAll( sub ), 'Expects {-sub-} as string/array of strings' );
+// 
+//   if( _.longIs( ins ) && _.longIs( sub ) )
+//   _.assert( ins.length === sub.length );
+// 
+//   ins = _.arrayAs( ins );
+//   for( let i = 0 ; i < ins.length ; i++ )
+//   _.assert( ins[ i ] !== '', '{-ins-} should be a string with length' );
+// 
+//   /* */
+// 
+//   let result = _.arrayAs( src ).slice();
+// 
+//   for( let k = 0 ; k < result.length ; k++ )
+//   result[ k ] = replaceRecurcive( result[ k ], 0 );
+// 
+//   if( result.length === 1 && _.strIs( src ) )
+//   return result[ 0 ];
+// 
+//   return result;
+// 
+//   /* */
+// 
+//   function replaceRecurcive( src, index )
+//   {
+//     for( let i = index ; i < ins.length ; i++ )
+//     {
+//       if( _.strHas( src, ins[ i ] ) )
+//       {
+//         let postfix = _.longIs( sub ) ? sub[ i ] : sub;
+//         
+//         let parts = src.split( ins[ i ] )
+//         if( index + 1 < ins.length )
+//         for( let i = 0 ; i < parts.length ; i++ )
+//         parts[ i ] = replaceRecurcive( parts[ i ], index + 1 )
+// 
+//         return parts.join( postfix );
+//       }
+//     }
+//     
+//     return src;  
+//   } 
+// 
+// }
 
 function strReplace( src, ins, sub ) /* qqq2 : ask */
 {
@@ -1001,43 +1049,75 @@ function strReplace( src, ins, sub ) /* qqq2 : ask */
 
   ins = _.arrayAs( ins );
   for( let i = 0 ; i < ins.length ; i++ )
-  if( ins[ i ] === '' )
-  _.assert( 0, '{-ins-} should be a string with length' );
+  _.assert( ins[ i ] !== '', '{-ins-} should be a string with length' );
 
   /* */
 
   let result = _.arrayAs( src ).slice();
 
   for( let k = 0 ; k < result.length ; k++ )
-  result[ k ] = replaceRecurcive( result[ k ], 0 );
+  {
+    let container = [ result[ k ] ];
+
+    for( let i = 0 ; i < ins.length ; i++ )
+    {
+      for( let l = 0 ; l < container.length ; l++ )
+      {
+        let insSrc = ins[ i ];
+        let src  = container[ l ];
+
+        if( _.strIs( container[ l ] ) && _.strHas( container[ l ], insSrc ) )
+        {
+          let index, ins;
+          if( _.regexpIs( insSrc ) ) 
+          {
+            let entry = insSrc.exec( src ); // Dmytro : single searching of substring
+            index = entry.index;  
+            ins = entry[ 0 ];
+          }
+          else
+          {
+            index = src.indexOf( insSrc );
+            ins = insSrc;
+          }
+
+          _.assert( ins !== '', '{-ins-} should find string with length' );
+
+          while( index !== -1 )
+          {
+            container.splice( l, 1, src.substring( 0, index ), i );
+            src = src.substring( index + ins.length );
+            l += 2;
+
+            if( _.regexpIs( insSrc ) )
+            {
+              let entry = insSrc.exec( src ); 
+              index = entry === null ? -1 : entry.index;  
+              ins = entry ? entry[ 0 ] : ins;
+              _.assert( ins !== '', '{-ins-} should find string with length' );
+            }
+            else
+            {
+              index = src.indexOf( insSrc );
+            }
+          }
+
+          container.splice( l, 0, src );
+        }
+      }
+    }
+
+    for( let j = 0 ; j < container.length ; j++ )
+    if( _.numberIs( container[ j ] ) )
+    container[ j ] = _.longIs( sub ) ? sub[ container[ j ] ] : sub;
+
+    result[ k ] = container.join( '' );
+  }
 
   if( result.length === 1 && _.strIs( src ) )
   return result[ 0 ];
 
   return result;
-
-  /* */
-
-  function replaceRecurcive( src, index )
-  {
-    for( let i = index ; i < ins.length ; i++ )
-    {
-      if( _.strHas( src, ins[ i ] ) )
-      {
-        let postfix = _.longIs( sub ) ? sub[ i ] : sub;
-        
-        let parts = src.split( ins[ i ] )
-        if( index + 1 < ins.length )
-        for( let i = 0 ; i < parts.length ; i++ )
-        parts[ i ] = replaceRecurcive( parts[ i ], index + 1 )
-
-        return parts.join( postfix );
-      }
-    }
-    
-    return src;  
-  } 
-
 }
 
 // --
