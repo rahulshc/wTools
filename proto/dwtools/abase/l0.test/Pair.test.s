e11c1a2c817bb6898278d584be64c9ef1dde7297
Dmytro <dm.vict.kr@gmail.com>
2020-05-21 13:58:24 +0300

    Fix test routines `pair` and `construct`, used valid long types

--- proto/dwtools/abase/l0.test/Pair.test.s
+++ proto/dwtools/abase/l0.test/Pair.test.s
@@ -149,7 +149,7 @@ function make( test )
   test.case = 'single argument - F32x';
   var src = new F32x([ 1, 3 ]);
   var got = _.pair.make( src );
-  var expected = _.longDescriptor.make([ 1, 3 ]);
+  var expected = new F32x([ 1, 3 ]);
   test.identical( got, expected );
   test.is( _.pair.is( got ) );
   test.is( src !== got );
@@ -229,7 +229,7 @@ function construct( test )
   test.case = 'single argument - F32x';
   var src = new F32x([ 1, 3 ]);
   var got = new _.pair( src );
-  var expected = _.longDescriptor.make([ 1, 3 ]);
+  var expected = new F32x([ 1, 3 ]);
   test.identical( got, expected );
   test.is( _.pair.is( got ) );
   test.is( src !== got );
