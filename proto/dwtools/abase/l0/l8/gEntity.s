ed12af77d701321dbbd516cea044e695dfb985f3
wandalen <wandalen.me@gmail.com>
2020-05-22 11:06:11 +0300

    .

--- proto/dwtools/abase/l0/l8/gEntity.s
+++ proto/dwtools/abase/l0/l8/gEntity.s
@@ -177,7 +177,6 @@ function entitySize( src )
     return;
 
     /* qqq : does not work anymore! */
-    debugger;
     if( it.down.iterable === 'map-like' || it.down.iterable === 'hash-map-like' )
     {
       result += _.uncountableSize( k );
