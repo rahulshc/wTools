4e882de3a00b995e5709ac1a10306fd1524d6a58
Jack Shell <jackshel45@gmail.com>
2020-08-28 21:32:52 +0300

    Wrote test routine `strSplitInlinedStereoOptionOnInlined`

--- proto/wtools/abase/l0.test/Str.test.s
+++ proto/wtools/abase/l0.test/Str.test.s
@@ -15258,6 +15258,101 @@ function strSplitInlinedStereoOptionPrefixPostfix( test )
 
 //
 
+
+function strSplitInlinedStereoOptionOnInlined( test )
+{
+  var onInlined = function( part )
+  {
+    var temp = part.split( ':' );
+
+    if( temp.length === 2 )
+    return temp;
+
+    return undefined;
+  };
+
+  /* - */
+  // ❮background:red❯
+
+  test.case = 'full split, closing delimeter';
+  var srcStr = 'this ❮background:red❯is❮background:default❯ text and is not';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    'this ', [ 'background', 'red' ], 'is', [ 'background', 'default' ], ' text and is not'
+  ];
+  test.identical( got, expected );
+
+  test.case = 'openning delimeter ❮ does not have closing';
+  var srcStr = 'this ❮background:red❯is❮background:default❯ text and ❮ is not';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    'this ', [ 'background', 'red' ], 'is', [ 'background', 'default' ], ' text and ❮ is not'
+  ];
+  test.identical( got, expected );
+
+  test.case = 'two inlined substrings is not in fact inlined';
+  var srcStr = '❮simple ❯ text ❮background:red❯is❮background:default❯ text and ❮ is not❯';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    '❮simple # text ', [ 'background', 'red' ], 'is', [ 'background', 'default' ], ' text and ❮ is not❯'
+  ];
+  test.identical( got, expected );
+
+  test.case = 'inlined at the beginning and false inlined';
+  var srcStr = '❮background:red❯i❮s❮background:default❯❮text';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    '', [ 'background', 'red' ], 'i❮s', [ 'background', 'default' ], '❮text'
+  ];
+  test.identical( got, expected );
+
+  test.case = 'inlined at the beginning and the end';
+  var srcStr = '❮background:red❯i❮s❮background:default❯';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    '', [ 'background', 'red' ], 'i❮s', [ 'background', 'default' ], ''
+  ];
+  test.identical( got, expected );
+
+  test.case = 'empty string left';
+  var srcStr = '❮❯ordinary❮inline2❯';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    '❮❯ordinary❮inline2❯'
+  ];
+  test.identical( got, expected );
+
+  test.case = 'empty string right';
+  var srcStr = '❮inline1❯ordinary❮❯';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    '❮inline1❯ordinary❮❯'
+  ];
+  test.identical( got, expected );
+
+  test.case = 'empty string middle';
+  var srcStr = '❮inline1❯❮inline2❯';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected =
+  [
+    '❮inline1❯❮inline2❯'
+  ];
+  test.identical( got, expected );
+
+  test.case = 'empty all';
+  var srcStr = '❮❯❮❯';
+  var got = _.strSplitInlinedStereo( { src : srcStr, onInlined : onInlined } );
+  var expected = [ '❮❯❮❯' ];
+  test.identical( got, expected );
+}
+
 // --
 // test suite
 // --
@@ -15341,6 +15436,7 @@ var Self =
     strSplitInlinedStereoOptionPrefixPostfix,
     strSplitInlinedStereoOptionStripping,
     strSplitInlinedStereoOptionPreservingEmpty,
+    strSplitInlinedStereoOptionOnInlined
 
   }
 
