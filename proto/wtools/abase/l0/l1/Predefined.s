43cfb691ba24320286ff44410e3dbb3dd9af0f70
wandalen <wandalen.me@gmail.com>
2020-10-30 07:46:44 +0200

    improve handling of ancaught errors

--- proto/wtools/abase/l0/l1/Predefined.s
+++ proto/wtools/abase/l0/l1/Predefined.s
@@ -23,6 +23,11 @@ if( !_global && typeof Global !== 'undefined' && Global.Global === Global ) _glo
 if( !_global && typeof global !== 'undefined' && global.global === global ) _global = global;
 if( !_global && typeof window !== 'undefined' && window.window === window ) _global = window;
 if( !_global && typeof self   !== 'undefined' && self.self === self ) _global = self;
+if( !_global._globals_ )
+{
+  _global._globals_ = Object.create( null );
+  _global._globals_.real = _global;
+}
 let _realGlobal = _global._realGlobal_ = _global;
 let _wasGlobal = _global._global_ || _global;
 _global = _wasGlobal;
@@ -32,14 +37,6 @@ _global._global_ = _wasGlobal;
 
 if( _global_.__GLOBAL_WHICH__ === 'real' )
 {
-
-  // if( _global_.wBase )
-  // {
-  //   if( _global_.wTools.usePath )
-  //   _global_.wTools.usePath( __dirname + '/../..' ); /* zzz : remove later */
-  //   module[ 'exports' ] = _global_.wBase;
-  //   return;
-  // }
 
   if( _global_.wBase )
   {
