92525e28865c7190c4742ee167462cb68d574658
Dmytro <dm.vict.kr@gmail.com>
2020-12-15 09:03:04 +0200

    .

--- sample/RoutineOptionsLowLevel.s
+++ sample/RoutineOptionsLowLevel.s
@@ -1,4 +1,6 @@
 
+/* minimal require */
+
 require( '../proto/wtools/abase/l0/l1/Predefined.s' );
 require( '../proto/wtools/abase/l0/l2/Setup.s' );
 require( '../proto/wtools/abase/l0/l3/Array.s' );
@@ -18,16 +20,16 @@ console.log( 'routineOptions, defaults in third argument' );
 var routine = () => true;
 var defaults = { a : null, b : 1 };
 var options = { a : 1, b : undefined };
-var got = _.routineOptions( routine, options, defaults );
-console.log( got );
+_.routineOptions( routine, options, defaults );
+console.log( options );
 /* log : { a : 1, b : 1 } */
 
 console.log( 'routineOptions, defaults in routine field' );
 var routine = () => true;
 routine.defaults = { a : null, b : 1 };
 var options = { a : 1, b : undefined };
-var got = _.routineOptions( routine, options );
-console.log( got );
+_.routineOptions( routine, options );
+console.log( options );
 /* log : { a : 1, b : 1 } */
 
 //
@@ -36,14 +38,14 @@ console.log( 'assertRoutineOptions, defaults in third argument' );
 var routine = () => true;
 var defaults = { a : null, b : 1 };
 var options = { a : 1, b : null };
-var got = _.assertRoutineOptions( routine, options, defaults );
-console.log( got );
+_.assertRoutineOptions( routine, options, defaults );
+console.log( options );
 /* log : { a : 1, b : null } */
 
 console.log( 'assertRoutineOptions, defaults in routine field' );
 var routine = () => true;
 routine.defaults = { a : null, b : 1 };
 var options = { a : 1, b : null };
-var got = _.assertRoutineOptions( routine, options );
-console.log( got );
+_.assertRoutineOptions( routine, options );
+console.log( options );
 /* log : { a : 1, b : null } */
