84fc9f25ca04918c0e773cd510b42f154845c854
wandalen <wandalen.me@gmail.com>
2021-01-28 23:45:33 +0200

    .

--- proto/wtools/abase/l0.test/Escape.test.s
+++ proto/wtools/abase/l0.test/Escape.test.s
@@ -23,26 +23,203 @@ function basic( test )
 
   /* */
 
-  test.case = '2 levels';
-  var src1 = _.escape.left( _.escape.left( null ) );
-  var src2 = _.escape.left( _.escape.left( null ) );
-  test.true( src1 !== src2 );
-  debugger;
-  test.identical( src1, src2 );
-  debugger;
-  test.identical( src2, src1 );
-  test.equivalent( src1, src2 );
-  test.equivalent( src2, src1 );
+  test.case = 'number';
+  var src = 13;
+  var exp = 13;
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
 
   /* */
 
-  // test.case = '3 levels';
-  // var src1 = _.escape.left( _.escape.left( _.escape.left( null ) ) );
-  // var src2 = _.escape.left( _.escape.left( _.escape.left( null ) ) );
-  // test.identical( src1, src2 );
-  // test.identical( src2, src1 );
-  // test.equivalent( src1, src2 );
-  // test.equivalent( src2, src1 );
+  test.case = 'null';
+  var src = null;
+  var exp = _.null;
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'undefined';
+  var src = undefined;
+  var exp = _.undefined;
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'nothing';
+  var src = _.nothing;
+  var exp = _.escape.nothing;
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'escaped1 null';
+  var src = _.null;
+  var exp = _.escape.null;
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'escaped1 undefined';
+  var src = _.undefined;
+  var exp = _.escape.undefined;
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'escaped1 nothing';
+  var src = _.nothing;
+  var exp = _.escape.nothing;
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'escaped2 null';
+  var src = _.escape.null;
+  var exp = new _.Escape( _.escape.null );
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'escaped2 undefined';
+  var src = _.escape.undefined;
+  var exp = new _.Escape( _.escape.undefined );
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'escaped2 nothing';
+  var src = _.escape.nothing;
+  var exp = new _.Escape( _.escape.nothing );
+  var left = _.escape.left( src );
+  test.identical( left, exp );
+  var right = _.escape.right( left );
+  test.identical( right, src );
+  test.true( right === src );
+
+  /* */
+
+}
+
+//
+
+function right( test )
+{
+
+  /* */
+
+  test.case = 'null';
+  var src = null;
+  var exp = null;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'undefined';
+  var src = undefined;
+  var exp = undefined;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'number';
+  var src = 13;
+  var exp = 13;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right === src );
+
+  /* */
+
+  test.case = 'nothing';
+  var src = _.nothing;
+  var exp = undefined;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right !== src );
+
+  /* */
+
+  test.case = 'escaped null';
+  var src = _.null;
+  var exp = null;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right !== src );
+
+  /* */
+
+  test.case = 'escaped undefined';
+  var src = _.undefined;
+  var exp = undefined;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right !== src );
+
+  /* */
+
+  test.case = 'escaped2 null';
+  var src = _.escape.null;
+  var exp = _.null;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right !== src );
+
+  /* */
+
+  test.case = 'escaped2 undefined';
+  var src = _.escape.undefined;
+  var exp = _.undefined;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right !== src );
+
+  /* */
+
+  test.case = 'escaped2 nothing';
+  var src = _.escape.nothing;
+  var exp = _.nothing;
+  var right = _.escape.right( src );
+  test.identical( right, exp );
+  test.true( right !== src );
 
   /* */
 
@@ -58,23 +235,40 @@ function equal( test )
   test.case = '2 levels';
   var src1 = _.escape.left( _.escape.left( null ) );
   var src2 = _.escape.left( _.escape.left( null ) );
-  test.true( src1 !== src2 );
-  debugger;
+  test.true( src1 === _.escape.null );
+  test.true( src1 === src2 );
   test.identical( src1, src2 );
-  debugger;
   test.identical( src2, src1 );
   test.equivalent( src1, src2 );
   test.equivalent( src2, src1 );
 
   /* */
 
-  // test.case = '3 levels';
-  // var src1 = _.escape.left( _.escape.left( _.escape.left( null ) ) );
-  // var src2 = _.escape.left( _.escape.left( _.escape.left( null ) ) );
-  // test.identical( src1, src2 );
-  // test.identical( src2, src1 );
-  // test.equivalent( src1, src2 );
-  // test.equivalent( src2, src1 );
+  test.case = '3 levels';
+  var src1 = _.escape.left( _.escape.left( _.escape.left( null ) ) );
+  var src2 = _.escape.left( _.escape.left( _.escape.left( null ) ) );
+  test.true( src1 !== _.escape.null );
+  test.true( src1 !== src2 );
+  test.true( src1.val.val === src2.val.val );
+  test.true( src1.val.val === _.null );
+  test.identical( src1, src2 );
+  test.identical( src2, src1 );
+  test.equivalent( src1, src2 );
+  test.equivalent( src2, src1 );
+
+  /* */
+
+  test.case = '4 levels';
+  var src1 = _.escape.left( _.escape.left( _.escape.left( _.escape.left( null ) ) ) );
+  var src2 = _.escape.left( _.escape.left( _.escape.left( _.escape.left( null ) ) ) );
+  test.true( src1 !== _.escape.null );
+  test.true( src1 !== src2 );
+  test.true( src1.val.val.val === src2.val.val.val );
+  test.true( src1.val.val.val === _.null );
+  test.identical( src1, src2 );
+  test.identical( src2, src1 );
+  test.equivalent( src1, src2 );
+  test.equivalent( src2, src1 );
 
   /* */
 
@@ -93,6 +287,8 @@ let Self =
   tests :
   {
 
+    basic,
+    right,
     equal,
 
   }
