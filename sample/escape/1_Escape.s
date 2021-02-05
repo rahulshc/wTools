9a1b0ffca4073d712c397481cd7610d6366d534a
Jack Shell <jackshel45@gmail.com>
2021-02-05 10:47:24 +0200

    Minor

--- sample/escape/1_Escape.s
+++ sample/escape/1_Escape.s
@@ -1,9 +1,2 @@
 let _ = require( '../..' );
 
-var left = _.escape.make( null );
-console.log( left.val === null );
-/* log : true */
-
-var left = _.escape.make( 12 );
-console.log( left.val === 12 );
-/* log : true */
