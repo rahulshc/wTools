9c0dcc49b827add9866ce49f4d94b66cb7e2cb56
Jack Shell <jackshel45@gmail.com>
2021-02-15 14:36:47 +0200

    Extend test routine `typingBasic`

--- proto/wtools/abase/l0.test/Constructible.test.s
+++ proto/wtools/abase/l0.test/Constructible.test.s
@@ -21,75 +21,98 @@ function typingBasic( test ) /* qqq for Yevhen : extend */
 
   test.case = 'str';
   var src = 'str';
+  test.true( !_.constructible.like( src ) );
   test.true( !_.constructible.is( src ) );
-  test.true( !_.objectIs( src ) );
   test.true( !_.objectLike( src ) );
-  test.true( !_.mapIs( src ) );
+  test.true( !_.objectIs( src ) );
   test.true( !_.mapLike( src ) );
+  test.true( !_.mapIs( src ) );
   test.true( !_.mapLikePrototyped( src ) );
   test.true( !_.mapIsPure( src ) );
-  test.true( !_.mapIsPolluted( src ) );
   test.true( !_.mapLikePolluted( src ) );
+  test.true( !_.mapIsPolluted( src ) );
 
   test.case = 'number';
   var src = 13;
+  test.true( !_.constructible.like( src ) );
   test.true( !_.constructible.is( src ) );
-  test.true( !_.objectIs( src ) );
   test.true( !_.objectLike( src ) );
-  test.true( !_.mapIs( src ) );
+  test.true( !_.objectIs( src ) );
   test.true( !_.mapLike( src ) );
+  test.true( !_.mapIs( src ) );
   test.true( !_.mapLikePrototyped( src ) );
   test.true( !_.mapIsPure( src ) );
-  test.true( !_.mapIsPolluted( src ) );
   test.true( !_.mapLikePolluted( src ) );
+  test.true( !_.mapIsPolluted( src ) );
+
+  /* */
+
+  test.case = `bool`;
+  var src = true;
+  test.true( !_.constructible.like( src ) );
+  test.true( !_.constructible.is( src ) );
+  test.true( !_.objectLike( src ) );
+  test.true( !_.objectIs( src ) );
+  test.true( !_.mapLike( src ) );
+  test.true( !_.mapIs( src ) );
+  test.true( !_.mapLikePrototyped( src ) );
+  test.true( !_.mapIsPure( src ) );
+  test.true( !_.mapLikePolluted( src ) );
+  test.true( !_.mapIsPolluted( src ) );
+
+  /* */
 
   test.case = 'pure map';
   var src = Object.create( null );
+  test.true( !_.constructible.like( src ) );
   test.true( !_.constructible.is( src ) );
-  test.true( _.objectIs( src ) );
   test.true( _.objectLike( src ) );
-  test.true( _.mapIs( src ) );
+  test.true( _.objectIs( src ) );
   test.true( _.mapLike( src ) );
+  test.true( _.mapIs( src ) );
   test.true( !_.mapLikePrototyped( src ) );
   test.true( _.mapIsPure( src ) );
-  test.true( !_.mapIsPolluted( src ) );
   test.true( !_.mapLikePolluted( src ) );
+  test.true( !_.mapIsPolluted( src ) );
 
   test.case = 'polluted map';
   var src = {};
+  test.true( !_.constructible.like( src ) );
   test.true( !_.constructible.is( src ) );
-  test.true( _.objectIs( src ) );
   test.true( _.objectLike( src ) );
-  test.true( _.mapIs( src ) );
+  test.true( _.objectIs( src ) );
   test.true( _.mapLike( src ) );
+  test.true( _.mapIs( src ) );
   test.true( !_.mapLikePrototyped( src ) );
   test.true( !_.mapIsPure( src ) );
-  test.true( _.mapIsPolluted( src ) );
   test.true( _.mapLikePolluted( src ) );
+  test.true( _.mapIsPolluted( src ) );
 
   test.case = 'regexp';
   var src = /abc/;
+  test.true( _.constructible.like( src ) );
   test.true( _.constructible.is( src ) );
-  test.true( !_.objectIs( src ) );
   test.true( _.objectLike( src ) );
-  test.true( !_.mapIs( src ) );
+  test.true( !_.objectIs( src ) );
   test.true( !_.mapLike( src ) );
+  test.true( !_.mapIs( src ) );
   test.true( !_.mapLikePrototyped( src ) );
   test.true( !_.mapIsPure( src ) );
-  test.true( !_.mapIsPolluted( src ) );
   test.true( !_.mapLikePolluted( src ) );
+  test.true( !_.mapIsPolluted( src ) );
 
   test.case = 'date';
   var src = new Date();
+  test.true( _.constructible.like( src ) );
   test.true( _.constructible.is( src ) );
-  test.true( !_.objectIs( src ) );
   test.true( _.objectLike( src ) );
-  test.true( !_.mapIs( src ) );
+  test.true( !_.objectIs( src ) );
   test.true( !_.mapLike( src ) );
+  test.true( !_.mapIs( src ) );
   test.true( !_.mapLikePrototyped( src ) );
   test.true( !_.mapIsPure( src ) );
-  test.true( !_.mapIsPolluted( src ) );
   test.true( !_.mapLikePolluted( src ) );
+  test.true( !_.mapIsPolluted( src ) );
 
 }
 
