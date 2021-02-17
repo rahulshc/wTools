43a82ec403c0a8e4877093dbf7d39f877894241c
Dmytro <dm.vict.kr@gmail.com>
2021-02-17 18:07:31 +0200

    Write test routine `bufferReusingResizeWithOptionOffsetting`

--- proto/wtools/abase/l0.test/Buffer.test.s
+++ proto/wtools/abase/l0.test/Buffer.test.s
@@ -15073,6 +15073,112 @@ function bufferReusingResizeDstIsBufferTyped( test )
   }
 }
 
+//
+
+function bufferReusingResizeWithOptionOffsetting( test )
+{
+  test.case = 'cinterval inside buffer, not changed length - not new buffer, dst === src, offsetting - 1';
+  var buffer = new U8x([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]).buffer;
+  var dst = new U8x( buffer, 2, 4 );
+  var got = _.bufferReusingResize
+  ({
+    dst,
+    src : dst,
+    cinterval : [ 0, 3 ],
+    offsetting : 1,
+    minSize : 1,
+  });
+  var expected = new U8x([ 2, 3, 4, 5 ]);
+  test.identical( got, expected );
+  test.true( got === dst );
+  test.true( got.buffer === dst.buffer );
+
+  test.case = 'cinterval inside buffer, not changed length - not new buffer, dst === src, offsetting - 0';
+  var buffer = new U8x([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]).buffer;
+  var dst = new U8x( buffer, 2, 4 );
+  var got = _.bufferReusingResize
+  ({
+    dst,
+    src : dst,
+    cinterval : [ 0, 3 ],
+    offsetting : 0,
+    minSize : 1,
+  });
+  var expected = new U8x([ 2, 3, 4, 5 ]);
+  test.identical( got, expected );
+  test.true( got === dst );
+  test.true( got.buffer === dst.buffer );
+
+  /* */
+
+  test.case = 'cinterval inside buffer, decrease length - new buffer, dst === src, offsetting - 1';
+  var buffer = new U8x([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]).buffer;
+  var dst = new U8x( buffer, 2, 4 );
+  var got = _.bufferReusingResize
+  ({
+    dst,
+    src : dst,
+    cinterval : [ 1, 3 ],
+    offsetting : 1,
+    minSize : 1,
+  });
+  var expected = new U8x([ 3, 4, 5 ]);
+  test.identical( got, expected );
+  test.true( got !== dst );
+  test.true( got.buffer === dst.buffer );
+
+  test.case = 'cinterval inside buffer, decrease length - new buffer, dst === src, offsetting - 0';
+  var buffer = new U8x([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]).buffer;
+  var dst = new U8x( buffer, 2, 4 );
+  var got = _.bufferReusingResize
+  ({
+    dst,
+    src : dst,
+    cinterval : [ 1, 3 ],
+    offsetting : 0,
+    minSize : 1,
+  });
+  var expected = new U8x([ 3, 4, 5 ]);
+  test.identical( got, expected );
+  test.true( got !== dst );
+  test.true( got.buffer === dst.buffer );
+
+  /* */
+
+  test.case = 'cinterval inside buffer, grow length - new buffer, dst === src, offsetting - 1';
+  var buffer = new U8x([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]).buffer;
+  var dst = new U8x( buffer, 2, 4 );
+  var got = _.bufferReusingResize
+  ({
+    dst,
+    src : dst,
+    cinterval : [ 0, 4 ],
+    offsetting : 1,
+    growFactor : 1,
+    minSize : 1,
+  });
+  var expected = new U8x([ 2, 3, 4, 5, 6 ]);
+  test.identical( got, expected );
+  test.true( got !== dst );
+  test.true( got.buffer === dst.buffer );
+
+  test.case = 'cinterval inside buffer, grow length - new buffer, dst === src, offsetting - 0';
+  var buffer = new U8x([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]).buffer;
+  var dst = new U8x( buffer, 2, 4 );
+  var got = _.bufferReusingResize
+  ({
+    dst,
+    src : dst,
+    cinterval : [ 0, 4 ],
+    offsetting : 0,
+    growFactor : 1,
+    minSize : 1,
+  });
+  var expected = new U8x([ 2, 3, 4, 5, 6 ]);
+  test.identical( got, expected );
+  test.true( got !== dst );
+  test.true( got.buffer !== dst.buffer );
+}
 
 //
 
@@ -19107,6 +19213,7 @@ let Self =
 
     bufferReusingResize,
     bufferReusingResizeDstIsBufferTyped,
+    bufferReusingResizeWithOptionOffsetting,
 
     //
 
