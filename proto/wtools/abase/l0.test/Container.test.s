093216f701011c9d79087757a4fe7274738184f6
Jack Shell <jackshel45@gmail.com>
2021-02-18 12:10:53 +0200

    Extend test routine `like`

--- proto/wtools/abase/l0.test/Container.test.s
+++ proto/wtools/abase/l0.test/Container.test.s
@@ -130,6 +130,249 @@ function like( test )
   test.identical( _.container.like( new WeakSet ), true );
   test.identical( _.container.like( new WeakSet([ { 'a' : 1 }, { 'b' : 2 } ]) ), true );
 
+  test.case = 'number';
+  var src = 1;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'bool & boolLike & fuzzy';
+  var src = true;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'boolLike & number & fuzzyLike';
+  var src = 1;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'fuzzy';
+  var src = _.maybe;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'bigint';
+  var src = 10n;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'str & regexpLike';
+  var src = 'str';
+  test.true( !_.container.like( src ) );
+
+  test.case = 'regexp & objectLike & constructible & constructibleLike';
+  var src = /hello/g;
+  test.true( _.container.like( src ) );
+
+  test.case = 'ArgumentsArray & arrayLike';
+  var src = _.argumentsArray.make();
+  test.true( _.container.like( src ) );
+
+  test.case = 'unroll';
+  var src = _.unrollMake([ 2, 3, 4 ]);
+  test.true( _.container.like( src ) );
+
+  test.case = 'array';
+  var src = [ 2, 3, 4 ];
+  test.true( _.container.like( src ) );
+
+  test.case = 'long & longLike';
+  var src = _.longMake([ 1, 2 ]);
+  test.true( _.container.like( src ) );
+
+  test.case = 'vector & vectorLike';
+  var src = new countableConstructor({ elements : [ '1', '10' ], withIterator : 1, length : 2 });
+  test.true( _.container.like( src ) );
+
+  test.case = 'countable & countableLike';
+  var src = new countableConstructor({ elements : [ '1', '10' ], withIterator : 1 });
+  test.true( _.container.like( src ) );
+
+  test.case = 'Global & GlobalReal';
+  var src = global;
+  test.true( _.container.like( src ) );
+
+  test.case = 'Global & GlobalDerived';
+  var src = Object.create( global );
+  test.true( _.container.like( src ) );
+
+  test.case = 'Object & ObjectLike & Container & ContainerLike';
+  var src = { [ Symbol.iterator ] : 1 };
+  test.true( _.container.like( src ) );
+
+  test.case = 'Object & ObjectLike & auxiliary & auxiliaryPrototyped & auxiliaryPolluted';
+  var src = { a : 1 };
+  Object.setPrototypeOf( src, { b : 2 } )
+  test.true( _.container.like( src ) );
+
+  test.case = 'Object & ObjectLike & auxiliary & map & mapPure';
+  var src = Object.create( null );
+  test.true( _.container.like( src ) );
+
+  test.case = 'Object & ObjectLike & auxiliary & auxiliaryPolluted & map & mapPolluted & mapPrototyped';
+  var src = {};
+  test.true( _.container.like( src ) );
+
+  test.case = 'HashMap';
+  var src = new HashMap();
+  test.true( _.container.like( src ) );
+
+  test.case = 'Set & SetLike';
+  var src = new Set();
+  test.true( _.container.like( src ) );
+
+  test.case = 'BufferNode';
+  var src = new BufferNode( 'str' );
+  test.true( !_.container.like( src ) );
+
+  test.case = 'BufferRaw';
+  var src = new BufferRaw( 'str' );
+  test.true( _.container.like( src ) );
+
+  test.case = 'BufferRawShared';
+  var src = new BufferRawShared( 'str' );
+  test.true( _.container.like( src ) );
+
+  test.case = 'BufferTyped';
+  var src = new I8x( 20 );
+  test.true( _.container.like( src ) );
+
+  test.case = 'BufferView';
+  var src = new BufferView( new BufferRaw( 20 ) )
+  test.true( _.container.like( src ) );
+
+  test.case = 'BufferBytes & BufferTyped';
+  var src = new U8x( 20 );
+  test.true( _.container.like( src ) );
+
+  test.case = 'err';
+  var src = _.err( 'error' );
+  test.true( _.container.like( src ) );
+
+  test.case = 'escape';
+  var src = _.escape.make( 1 );
+  test.true( _.container.like( src ) );
+
+  test.case = 'interval & BufferTyped';
+  var src = _.escape.make( new F32x( 2 ) );
+  test.true( _.container.like( src ) );
+
+  test.case = 'pair';
+  var src = _.pair.make();
+  test.true( _.container.like( src ) );
+
+  test.case = 'path & str';
+  var src = '/a/b/';
+  test.true( !_.container.like( src ) );
+
+  test.case = 'propertyTransformer & filter';
+  var src = _.property.filter[ 'dstAndSrcOwn' ];
+  test.true( !_.container.like( src ) );
+
+  test.case = 'propertyTransformer & mapper';
+  var src = _.property.mapper[ 'assigning' ];
+  test.true( !_.container.like( src ) );
+
+  test.case = 'routine & routineLike';
+  var src = routine;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'timer';
+  var src = _.time._begin( Infinity );;
+  test.true( _.container.like( src ) );
+  _.time.cancel( src );
+
+  test.case = 'date & objectLike';
+  var src = new Date();
+  test.true( _.container.like( src ) );
+
+  test.case = 'null';
+  var src = null;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'undefined';
+  var src = undefined;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'Symbol null';
+  var src = _.null;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'Symbol undefined';
+  var src = _.undefined;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'Symbol Nothing';
+  var src = _.nothing;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'primitive';
+  var src = 5;
+  test.true( !_.container.like( src ) );
+
+  test.case = 'Symbol';
+  var src = Symbol( 'a' );
+  test.true( !_.container.like( src ) );
+
+  test.case = 'ConsequenceLike & promiseLike & promise';
+  var src = new Promise( ( resolve, reject ) => { return resolve( 0 ) } );
+  test.true( _.container.like( src ) );
+
+  test.case = 'stream';
+  var src = require( 'stream' ).Readable();
+  test.true( _.container.like( src ) );
+
+  test.case = 'console';
+  var src = console;
+  test.true( _.container.like( src ) );
+
+  test.case = 'printerLike';
+  var src = _global.logger;
+  test.true( _.container.like( src ) );
+
+  test.case = 'process';
+  var src = process;
+  test.true( _.container.like( src ) );
+
+  /* - */
+
+  function _iterate()
+  {
+
+    let iterator = Object.create( null );
+    iterator.next = next;
+    iterator.index = 0;
+    iterator.instance = this;
+    return iterator;
+
+    function next()
+    {
+      let result = Object.create( null );
+      result.done = this.index === this.instance.elements.length;
+      if( result.done )
+      return result;
+      result.value = this.instance.elements[ this.index ];
+      this.index += 1;
+      return result;
+    }
+
+  }
+
+  /* */
+
+  function countableConstructor( o )
+  {
+    return countableMake( this, o );
+  }
+
+  /* */
+
+  function countableMake( dst, o )
+  {
+    if( dst === null )
+    dst = Object.create( null );
+    _.mapExtend( dst, o );
+    if( o.withIterator )
+    dst[ Symbol.iterator ] = _iterate;
+    return dst;
+  }
+
+  function routine () {}
+
 }
 
 //
