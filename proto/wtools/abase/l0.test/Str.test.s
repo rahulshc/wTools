d1cfaee105e4ee58e8af36ce419a78bad6813e77
Jack Shell <jackshel45@gmail.com>
2021-02-19 13:31:49 +0200

    Fixed to pass tests

--- proto/wtools/abase/l0.test/Str.test.s
+++ proto/wtools/abase/l0.test/Str.test.s
@@ -3182,98 +3182,298 @@ function strEnds( test )
 
 function strEntityShort( test )
 {
+
   test.case = 'number';
-  var src = 13;
-  var expected = '13';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
-
-  /* ? */
-  // test.case = 'numberLike';
-  // var src = 13;
-  // var expected = '13';
-  // var got = _.strEntityShort( src );
-  // test.identical( got, expected );
-
-  test.case = 'bool';
-  var src = true;
-  var expected = 'true';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
-
-  /* ? */
-  test.case = 'boolLike';
   var src = 1;
   var expected = '1';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  test.identical( _.strEntityShort( src ), expected );
 
-  /* ? */
+  test.case = 'bool & boolLike & fuzzy';
+  var src = true;
+  var expected = 'true';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'boolLike & number & fuzzyLike';
+  var src = 0;
+  var expected = '0';
+  test.identical( _.strEntityShort( src ), expected );
+
   test.case = 'fuzzy';
   var src = _.maybe;
   var expected = '{- Symbol maybe -}';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
-
-  /* ? */
-  // test.case = 'fuzzyLike';
-  // var src = 1;
-  // var expected = '1';
-  // var got = _.strEntityShort( src );
-  // test.identical( got, expected );
+  test.identical( _.strEntityShort( src ), expected );
 
   test.case = 'bigint';
-  var src = 13n;
-  var expected = '13';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  var src = 10n;
+  var expected = '10';
+  test.identical( _.strEntityShort( src ), expected );
 
-  test.case = 'string';
-  var src = 'abc';
-  var expected = 'abc';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  test.case = 'str & regexpLike';
+  var src = 'str';
+  var expected = 'str';
+  test.identical( _.strEntityShort( src ), expected );
 
-  test.case = 'regexp';
-  var src = /abc/i;
-  var expected = '/abc/i';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  test.case = 'regexp & objectLike & constructible & constructibleLike';
+  var src = /hello/g;
+  var expected = '/hello/g';
+  test.identical( _.strEntityShort( src ), expected );
 
+  test.case = 'ArgumentsArray & arrayLike';
+  var src = _.argumentsArray.make();
+  var expected = '{- ArgumentsArray with 0 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
 
+  test.case = 'unroll';
+  var src = _.unrollMake([ 2, 3, 4 ]);
+  var expected = '{- Array with 3 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
 
+  test.case = 'array';
+  var src = [ 2, 3, 4 ];
+  var expected = '{- Array with 3 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
 
+  test.case = 'long & longLike';
+  var src = _.longMake([ 1, 2 ]);
+  var expected = '{- Array with 2 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
 
-  test.case = 'undefined';
-  var src = undefined;
-  var expected = 'undefined';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  test.case = 'vector & vectorLike';
+  var src = new countableConstructor({ elements : [ '1', '10' ], withIterator : 1, length : 2 });
+  var expected = '{- countableConstructor.countable with 2 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'countable & countableLike';
+  var src = new countableConstructor({ elements : [ '1', '10' ], withIterator : 1 });
+  var expected = '{- countableConstructor.countable.constructible with 2 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'Global & GlobalReal';
+  var src = global;
+  var expected = '{- MapLike.polluted.prototyped with ';
+  test.true( _.strHas( _.strEntityShort( src ), expected ) );
+
+  test.case = 'Global & GlobalDerived';
+  var src = Object.create( global );
+  var expected = '{- MapLike.polluted.prototyped with ';
+  test.true( _.strHas( _.strEntityShort( src ), expected ) );
+
+  test.case = 'Object & ObjectLike & Container & ContainerLike';
+  var src = { [ Symbol.iterator ] : 1 };
+  var expected = '{- Object with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'Object & ObjectLike & auxiliary & auxiliaryPrototyped & auxiliaryPolluted';
+  var src = { a : 1 };
+  Object.setPrototypeOf( src, { b : 2 } )
+  var expected = '{- MapLike.polluted.prototyped with 2 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'Object & ObjectLike & auxiliary & map & mapPure';
+  var src = Object.create( null );
+  var expected = '{- Map.pure with 0 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'Object & ObjectLike & auxiliary & auxiliaryPolluted & map & mapPolluted & mapPrototyped';
+  var src = {};
+  var expected = '{- Map.polluted with 0 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'HashMap';
+  var src = new HashMap();
+  var expected = '{- HashMap with 0 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'Set & SetLike';
+  var src = new Set();
+  var expected = '{- Set with 0 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'BufferNode';
+  var src = new BufferNode( 'str' );
+  var expected = '{- BufferNode with 3 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'BufferRaw';
+  var src = new BufferRaw( 'str' );
+  var expected = '{- BufferRaw with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'BufferRawShared';
+  var src = new BufferRawShared( 'str' );
+  var expected = '{- BufferRawShared with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'BufferTyped';
+  var src = new I8x( 20 );
+  var expected = '{- I8x with 20 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'BufferView';
+  var src = new BufferView( new BufferRaw( 20 ) );
+  var expected = '{- DataView.constructible with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'BufferBytes & BufferTyped';
+  var src = new U8x( 20 );
+  var expected = '{- U8x with 20 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'err';
+  var src = _.err( 'error' );
+  var expected = '{- Error.constructible with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'escape';
+  var src = _.escape.make( 1 );
+  var expected = 'Escape( 1 )';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'interval & BufferTyped';
+  var src = new F32x( 2 );
+  var expected = '{- F32x with 2 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'pair';
+  var src = _.pair.make();
+  var expected = '{- Array with 2 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'path & str';
+  var src = '/a/b/';
+  var expected = '/a/b/';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'propertyTransformer & filter';
+  var src = _.property.filter[ 'dstAndSrcOwn' ];
+  var expected = '{- routine dstAndSrcOwn -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'propertyTransformer & mapper';
+  var src = _.property.mapper[ 'assigning' ];
+  var expected = '{- routine assigning -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'routine & routineLike';
+  var src = routine;
+  var expected = '{- routine routine -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'timer';
+  var src = _.time._begin( Infinity );
+  var expected = '{- Map.pure with 9 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+  _.time.cancel( src );
+
+  test.case = 'date & objectLike';
+  var src = new Date( '2021-02-19T11:26:42.840Z' );
+  var expected = '2021-02-19T11:26:42.840Z';
+  test.identical( _.strEntityShort( src ), expected );
 
   test.case = 'null';
   var src = null;
   var expected = 'null';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  test.identical( _.strEntityShort( src ), expected );
 
-  test.case = 'date';
-  var src = new Date( Date.UTC( 2016, 12, 8 ) );
-  var expected = '2017-01-08T00:00:00.000Z';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  test.case = 'undefined';
+  var src = undefined;
+  var expected = 'undefined';
+  test.identical( _.strEntityShort( src ), expected );
 
-  test.case = 'routine';
-  var src = abc;
-  var expected = '{- routine abc -}';
-  var got = _.strEntityShort( src );
-  test.identical( got, expected );
+  test.case = 'Symbol null';
+  var src = _.null;
+  var expected = '{- Symbol null -}';
+  test.identical( _.strEntityShort( src ), expected );
 
-  test.case = 'anonymous routine';
-  var expected = '{- routine.anonymous -}';
-  var got = _.strEntityShort( function(){} );
-  test.identical( got, expected );
+  test.case = 'Symbol undefined';
+  var src = _.undefined;
+  var expected = '{- Symbol undefined -}';
+  test.identical( _.strEntityShort( src ), expected );
 
-  function abc(){}
+  test.case = 'Symbol Nothing';
+  var src = _.nothing;
+  var expected = '{- Symbol nothing -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'primitive';
+  var src = 5;
+  var expected = '5';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'Symbol';
+  var src = Symbol( 'a' );
+  var expected = '{- Symbol a -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'ConsequenceLike & promiseLike & promise';
+  var src = new Promise( ( resolve, reject ) => { return resolve( 0 ) } );
+  var expected = '{- Promise.constructible with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'stream';
+  var src = require( 'stream' ).Readable();
+  var expected = '{- Readable.constructible with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'console';
+  var src = console;
+  var expected = '{- Console.constructible with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'printerLike';
+  var src = _global.logger;
+  var expected = '{- Map.polluted with 9 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  test.case = 'process';
+  var src = process;
+  var expected = '{- process.constructible with 1 elements -}';
+  test.identical( _.strEntityShort( src ), expected );
+
+  /* - */
+
+  function _iterate()
+  {
+
+    let iterator = Object.create( null );
+    iterator.next = next;
+    iterator.index = 0;
+    iterator.instance = this;
+    return iterator;
+
+    function next()
+    {
+      let result = Object.create( null );
+      result.done = this.index === this.instance.elements.length;
+      if( result.done )
+      return result;
+      result.value = this.instance.elements[ this.index ];
+      this.index += 1;
+      return result;
+    }
+
+  }
+
+  /* */
+
+  function countableConstructor( o )
+  {
+    return countableMake( this, o );
+  }
+
+  /* */
+
+  function countableMake( dst, o )
+  {
+    if( dst === null )
+    dst = Object.create( null );
+    _.mapExtend( dst, o );
+    if( o.withIterator )
+    dst[ Symbol.iterator ] = _iterate;
+    return dst;
+  }
+
+  function routine () {}
 
 }
 
