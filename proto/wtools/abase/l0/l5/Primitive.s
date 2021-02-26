6b4b9f103fd3c9cad602ed12c6f741969728e7fc
Jack Shell <jackshel45@gmail.com>
2021-02-26 15:05:35 +0200

    Created exportString routines for primitive, symbol

--- proto/wtools/abase/l0/l5/Primitive.s
+++ proto/wtools/abase/l0/l5/Primitive.s
@@ -11,6 +11,36 @@ let Self = _.primitive = _.primitive || Object.create( null );
 // primitive
 // --
 
+function exportStringShortCode( src )
+{
+  _.assert( arguments.length === 1, 'Expects exactly one argument' );
+  _.assert( _.primitive.is( src ) );
+
+  if( _.symbol.is( src ) )
+  return _.symbol.exportStringShortCode( src );
+
+  if( _.bigInt.is( src ) )
+  return _.bigInt.exportStringShortCode( src );
+
+  return String( src );
+}
+
+//
+
+function exportStringShortDiagnostic( src )
+{
+  _.assert( arguments.length === 1, 'Expects exactly one argument' );
+  _.assert( _.primitive.is( src ) );
+
+  if( _.symbol.is( src ) )
+  return _.symbol.exportStringShortDiagnostic( src );
+
+  if( _.bigInt.is( src ) )
+  return _.bigInt.exportStringShortDiagnostic( src );
+
+  return String( src );
+}
+
 // --
 // extension
 // --
@@ -24,7 +54,12 @@ let ExtensionTools =
 
 let Extension =
 {
-
+  exportString : exportStringShortDiagnostic,
+  exportStringShort : exportStringShortDiagnostic,
+  exportStringShortCode,
+  exportStringShortDiagnostic,
+  exportStringDiagnostic : exportStringShortDiagnostic,
+  exportStringCode : exportStringShortCode
 }
 
 Object.assign( _, ExtensionTools );
