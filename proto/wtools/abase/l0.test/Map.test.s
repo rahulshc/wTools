25910803e987cd1e31f13df4808b1e483a319c2c
dmvict <dm.vict.kr@gmail.com>
2021-03-03 08:13:07 +0200

    Write test routine `mapOnlyOwn_ScreenMapIsVector`

--- proto/wtools/abase/l0.test/Map.test.s
+++ proto/wtools/abase/l0.test/Map.test.s
@@ -10889,6 +10889,142 @@ function mapOnlyOwn_SrcMapsIsVector( test )
 
 //
 
+function mapOnlyOwn_ScreenMapIsVector( test )
+{
+  test.open( 'unroll' );
+
+  test.case = 'srcMap - empty map, screenMap - empty vector';
+  var srcMap = {};
+  var screenMap = _.unrollMake( [] );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, {} );
+  test.identical( screenMap, _.unrollMake( [] ) );
+
+  test.case = 'srcMap - empty map, screenMap - filled vector';
+  var srcMap = {};
+  var screenMap = _.unrollMake([ 'a', 'c', { b : 77 } ]);
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, {} );
+  test.identical( screenMap, _.unrollMake([ 'a', 'c', { b : 77 } ]) );
+
+  test.case = 'screenMap - empty vector';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.unrollMake( [] );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.unrollMake( [] ) );
+
+  test.case = 'all keys in srcMap exists in screenMap - array';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.unrollMake([ 'a', 'c', { d : 7 } ]);
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = { a : 'abc', c : 33, d : 'name' };
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.unrollMake([ 'a', 'c', { d : 7 } ]) );
+
+  test.case = 'none keys in srcMap exists in screenMap - array';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.unrollMake([ 'aa', '13', { bb : 77 } ]);
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.unrollMake([ 'aa', '13', { bb : 77 } ]) );
+
+  test.case = 'srcMap has numerical keys, screenMap has not primitives';
+  var srcMap = { 0 : 'name', 1 : 33, 2 : 'abc' };
+  var screenMap = _.unrollMake([ { a : 13 }, [ 'a', 'b', 'c' ] ]);
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = { 0 : 'name', 1 : 33, 2 : 'abc' };
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { 0 : 'name', 1 : 33, 2 : 'abc' } );
+  test.identical( screenMap, _.unrollMake([ { a : 13 }, [ 'a', 'b', 'c' ] ]) );
+
+  test.close( 'unroll' );
+
+  /* - */
+
+
+  test.open( 'containerAdapter' );
+
+  test.case = 'srcMap - empty map, screenMap - empty vector';
+  var srcMap = {};
+  var screenMap = _.containerAdapter.make( new Set( [] ) );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, {} );
+  test.identical( screenMap, _.containerAdapter.make( new Set( [] ) ) );
+
+  test.case = 'srcMap - empty map, screenMap - filled vector';
+  var srcMap = {};
+  var screenMap = _.containerAdapter.make( new Set([ 'a', 'c', { b : 77 } ]) );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, {} );
+  test.identical( screenMap, _.containerAdapter.make( new Set([ 'a', 'c', { b : 77 } ]) ) );
+
+  test.case = 'screenMap - empty vector';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.containerAdapter.make( new Set( [] ) );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.containerAdapter.make( new Set( [] ) ) );
+
+  test.case = 'all keys in srcMap exists in screenMap - array';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.containerAdapter.make( new Set([ 'a', 'c', { d : 7 } ]) );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = { a : 'abc', c : 33, d : 'name' };
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.containerAdapter.make( new Set([ 'a', 'c', { d : 7 } ]) ) );
+
+  test.case = 'none keys in srcMap exists in screenMap - array';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.containerAdapter.make( new Set([ 'aa', '13', { bb : 77 } ]) );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.containerAdapter.make( new Set([ 'aa', '13', { bb : 77 } ]) ) );
+
+  test.case = 'srcMap has numerical keys, screenMap has not primitives';
+  var srcMap = { 0 : 'name', 1 : 33, 2 : 'abc' };
+  var screenMap = _.containerAdapter.make( new Set([ { a : 13 }, [ 'a', 'b', 'c' ] ]) );
+  var got = _.mapOnlyOwn_( null, srcMap, screenMap );
+  var expected = { 0 : 'name', 1 : 33, 2 : 'abc' };
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { 0 : 'name', 1 : 33, 2 : 'abc' } );
+  test.identical( screenMap, _.containerAdapter.make( new Set([ { a : 13 }, [ 'a', 'b', 'c' ] ]) ) );
+
+  test.close( 'containerAdapter' );
+}
+
+//
+
 function mapOnlyComplementing_WithTwoArguments( test )
 {
   test.open( 'srcMap - map' );
@@ -15427,6 +15563,7 @@ let Self =
     mapOnlyOwn_DstMapIsNull,
     mapOnlyOwn_DstMapIsMap,
     mapOnlyOwn_SrcMapsIsVector,
+    mapOnlyOwn_ScreenMapIsVector,
 
     mapOnlyComplementing_WithTwoArguments,
     mapOnlyComplementing_DstMapIsNull,
