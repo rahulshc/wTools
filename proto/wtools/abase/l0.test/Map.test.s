338c2b7012397c23492d902e73d07e042b277ca0
dmvict <dm.vict.kr@gmail.com>
2021-03-03 07:42:50 +0200

    Write test routine `mapOnlyScreenMapIsVector`

--- proto/wtools/abase/l0.test/Map.test.s
+++ proto/wtools/abase/l0.test/Map.test.s
@@ -8710,6 +8710,71 @@ function mapOnlySrcMapsIsVector( test )
 
 //
 
+function mapOnlyScreenMapIsVector( test )
+{
+  test.case = 'srcMap - empty map, screenMap - empty vector';
+  var srcMap = {};
+  var screenMap = _.unrollMake([]);
+  var got = _.mapOnly( srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, {} );
+  test.identical( screenMap, _.unrollMake( [] ) );
+
+  test.case = 'srcMap - empty map, screenMap - filled vector';
+  var srcMap = {};
+  var screenMap = _.unrollMake([ 'a', 'c', { b : 77 } ]);
+  var got = _.mapOnly( srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, {} );
+  test.identical( screenMap, _.unrollMake([ 'a', 'c', { b : 77 } ]) );
+
+  test.case = 'screenMap - empty vector';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.unrollMake( [] );
+  var got = _.mapOnly( srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.unrollMake( [] ) );
+
+  test.case = 'all keys in srcMap exists in screenMap - array';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.unrollMake([ 'a', 'c', { d : 7 } ]);
+  var got = _.mapOnly( srcMap, screenMap );
+  var expected = { a : 'abc', c : 33, d : 'name' };
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.unrollMake([ 'a', 'c', { d : 7 } ]) );
+
+  test.case = 'none keys in srcMap exists in screenMap - array';
+  var srcMap = { d : 'name', c : 33, a : 'abc' };
+  var screenMap = _.unrollMake([ 'aa', '13', { bb : 77 } ]);
+  var got = _.mapOnly( srcMap, screenMap );
+  var expected = {};
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { d : 'name', c : 33, a : 'abc' } );
+  test.identical( screenMap, _.unrollMake([ 'aa', '13', { bb : 77 } ]) );
+
+  test.case = 'srcMap has numerical keys, screenMap has not primitives';
+  var srcMap = { 0 : 'name', 1 : 33, 2 : 'abc' };
+  var screenMap = _.unrollMake([ { a : 13 }, [ 'a', 'b', 'c' ] ]);
+  var got = _.mapOnly( srcMap, screenMap );
+  var expected = { 0 : 'name', 1 : 33, 2 : 'abc' };
+  test.identical( got, expected );
+  test.true( got !== srcMap );
+  test.identical( srcMap, { 0 : 'name', 1 : 33, 2 : 'abc' } );
+  test.identical( screenMap, _.unrollMake([ { a : 13 }, [ 'a', 'b', 'c' ] ]) );
+}
+
+//
+
 function mapOnlyTwoArguments_( test )
 {
   test.open( 'srcMap - map' );
@@ -14913,6 +14978,7 @@ let Self =
 
     mapOnly,
     mapOnlySrcMapsIsVector,
+    mapOnlyScreenMapIsVector,
 
     mapOnlyTwoArguments_,
     mapOnlyDstMapIsNull_,
