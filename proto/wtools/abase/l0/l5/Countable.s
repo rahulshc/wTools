ad2389430fbe5c60ec31d3cea724a920336ba028
Jack Shell <jackshel45@gmail.com>
2021-03-09 15:58:35 +0200

    Write test routine `equivalent`

--- proto/wtools/abase/l0/l5/Countable.s
+++ proto/wtools/abase/l0/l5/Countable.s
@@ -38,6 +38,32 @@ function identicalShallow( src1, src2 )
 
 //
 
+function equivalentShallow( src1, src2 )
+{
+  _.assert( arguments.length === 2, 'Expects exactly two arguments' );
+  _.assert( _.countable.is( src1 ) );
+  _.assert( _.countable.is( src2 ) );
+
+  let length1 = src1.length || 0;
+  let length2 = src2.length || 0;
+
+  if( length1 !== length2 )
+  return false;
+
+  if( _.longLike( src1 ) && _.longLike( src2 ) )
+  {
+    return _.longIdenticalShallow( src1, src2 );
+  }
+
+  /*
+    object with method iterator,
+    vector
+  */
+  return _.mapsAreIdentical( src1, src2 )
+}
+
+//
+
 function exportStringShortDiagnostic( src )
 {
   _.assert( arguments.length === 1, 'Expects exactly one argument' );
@@ -56,7 +82,7 @@ function exportStringShortDiagnostic( src )
 var Extension =
 {
   identicalShallow,
-  equivalentShallow : identicalShallow,
+  equivalentShallow,
   exportString : exportStringShortDiagnostic,
   exportStringShort : exportStringShortDiagnostic,
   exportStringShortDiagnostic,
