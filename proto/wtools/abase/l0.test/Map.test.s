0b608096ee1059b6d248969389d9e446867818e9
Yevhen Shulha <jackshel45@gmail.com>
2021-04-14 19:17:06 +0300

    Extending tests

--- proto/wtools/abase/l0.test/Map.test.s
+++ proto/wtools/abase/l0.test/Map.test.s
@@ -20513,6 +20513,184 @@ function mapsExtendAppendingOnceRecursiveCountable( test )
   }
 }
 
+//
+
+function mapSupplementByMapsRecursiveCountable( test )
+{
+
+  test.case = 'all new fields in 1 obj';
+  var dst = { a : 1, b : 2 };
+  var got = _.mapSupplementByMapsRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5, c : 3 } ], withIterator : 1 })
+  );
+  var expected = { a : 1, b : 2, c : 3, d : 5 };
+  test.identical( got, expected );
+
+  test.case = 'all new fields in different objects';
+  var dst = { a : 1, b : 2 };
+  var got = _.mapSupplementByMapsRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5 }, { c : 3 } ], withIterator : 1 })
+  );
+  var expected = { a : 1, b : 2, c : 3, d : 5 };
+  test.identical( got, expected );
+
+  test.case = 'new fields in 1 obj';
+  var dst = { a : 1, b : 2 };
+  var got = _.mapSupplementByMapsRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5, c : 3, a : 2 } ], withIterator : 1 })
+  );
+  var expected = { a : 1, b : 2, c : 3, d : 5 };
+  test.identical( got, expected );
+
+  test.case = 'new fields in different objects';
+  var dst = { a : 1, b : 2, c : 3 };
+  var got = _.mapSupplementByMapsRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5 }, { c : 3 }, { a : 2 } ], withIterator : 1 })
+  );
+  var expected = { a : 1, b : 2, c : 3, d : 5 };
+  test.identical( got, expected );
+
+  /* - */
+
+  function _iterate()
+  {
+
+    let iterator = Object.create( null );
+    iterator.next = next;
+    iterator.index = 0;
+    iterator.instance = this;
+    return iterator;
+
+    function next()
+    {
+      let result = Object.create( null );
+      result.done = this.index === this.instance.elements.length;
+      if( result.done )
+      return result;
+      result.value = this.instance.elements[ this.index ];
+      this.index += 1;
+      return result;
+    }
+
+  }
+
+  /* */
+
+  function countableConstructor( o )
+  {
+    return countableMake( this, o );
+  }
+
+  /* */
+
+  function countableMake( dst, o )
+  {
+    if( dst === null )
+    dst = Object.create( null );
+    _.mapExtend( dst, o );
+    if( o.withIterator )
+    dst[ Symbol.iterator ] = _iterate;
+    return dst;
+  }
+}
+
+//
+
+function mapsSupplementOwnRecursiveCountable( test )
+{
+
+  test.case = 'all new fields in 1 obj';
+  var dst = { a : 1, b : 2 };
+  var got = _.mapsSupplementOwnRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5, c : 3 } ], withIterator : 1 })
+  );
+  var expected = { a : 1, b : 2 };
+  test.identical( got, expected );
+
+  test.case = 'all new fields in different objects';
+  var dst = { a : 1, b : 2 };
+  var got = _.mapsSupplementOwnRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5 }, { c : 3 } ], withIterator : 1 })
+  );
+  var expected = { a : 1, b : 2 };
+  test.identical( got, expected );
+
+  test.case = 'new fields in 1 obj';
+  var dst = { a : 1, b : 2 };
+  var got = _.mapsSupplementOwnRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5, c : 3, a : 2 } ], withIterator : 1 })
+  );
+  var expected = { a : 2, b : 2 };
+  test.identical( got, expected );
+
+  test.case = 'new fields in different objects';
+  var dst = { a : 2, b : 2, c : 3 };
+  var got = _.mapsSupplementOwnRecursive
+  (
+    dst,
+    new countableConstructor({ elements : [ { d : 5 }, { c : 3 }, { a : 2 } ], withIterator : 1 })
+  );
+  var expected = { a : 2, b : 2, c : 3 };
+  test.identical( got, expected );
+
+  /* - */
+
+  function _iterate()
+  {
+
+    let iterator = Object.create( null );
+    iterator.next = next;
+    iterator.index = 0;
+    iterator.instance = this;
+    return iterator;
+
+    function next()
+    {
+      let result = Object.create( null );
+      result.done = this.index === this.instance.elements.length;
+      if( result.done )
+      return result;
+      result.value = this.instance.elements[ this.index ];
+      this.index += 1;
+      return result;
+    }
+
+  }
+
+  /* */
+
+  function countableConstructor( o )
+  {
+    return countableMake( this, o );
+  }
+
+  /* */
+
+  function countableMake( dst, o )
+  {
+    if( dst === null )
+    dst = Object.create( null );
+    _.mapExtend( dst, o );
+    if( o.withIterator )
+    dst[ Symbol.iterator ] = _iterate;
+    return dst;
+  }
+}
+
 // --
 // define test suite
 // --
@@ -20733,15 +20911,21 @@ const Proto =
     mapsExtendNullsCountable,
     mapsExtendDstNotOwnCountable,
     mapsExtendNotIdenticalCountable,
+
     mapsSupplementAppendingCountable,
+
     mapsComplementCountable,
     mapsComplementReplacingUndefinesCountable,
     mapsComplementPreservingUndefinesCountable,
+
     mapsExtendRecursiveConditionalCountable,
     mapsExtendRecursiveCountable,
     mapsExtendAppendingAnythingRecursiveCountable,
     mapsExtendAppendingArraysRecursiveCountable,
     mapsExtendAppendingOnceRecursiveCountable,
+
+    mapSupplementByMapsRecursiveCountable,
+    mapsSupplementOwnRecursiveCountable,
   }
 }
 
