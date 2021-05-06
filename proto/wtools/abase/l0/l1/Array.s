ef428aee364204df9e429513f3b5fe36023f08dc
dmvict <dm.vict.kr@gmail.com>
2021-05-06 16:27:48 +0300

    .

--- proto/wtools/abase/l0/l1/Array.s
+++ proto/wtools/abase/l0/l1/Array.s
@@ -185,9 +185,19 @@ function _make( src, length )
   {
     let data = length;
     if( _.number.is( length ) )
-    data = src;
+    {
+      data = src;
+    }
+    else if( length.length )
+    {
+      length = length.length;
+    }
     if( _.countable.is( length ) )
-    length = length.length;
+    {
+      data = [ ... length ];
+      length = data.length;
+    }
+
     return fill( new Array( length ), data );
   }
   else if( arguments.length === 1 )
